dbc6642705d7259f2213c6b00c22cfe091aaa98a
Nicolas Cadena <nicolas.cadena@globant.com>
2020-06-15 17:10:38 -0500

    Uncommented test_load_deliversSuccessWithNoItemsOn200HTTPResponseWithEmptyJSONList(), added Root and Item structs to handle decoding of data although just passing an empty array for now.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,15 +21,40 @@ public final class RemoteFeedLoader: FeedLoader {
     private struct StatusCodeConstants {
         static var code200 = 200
     }
+    
+    private struct Root: Decodable {
+        let items: [Item]
+        
+        var feed: [FeedImage] {
+            return items.map { $0.item }
+        }
+    }
+
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedImage {
+            return FeedImage(id: id,
+                             description: description, location: location,
+                             url: image)
+        }
+    }
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { (result) in
             switch result {
-            case let .success((_, response)):
+            case let .success((data, response)):
                 if response.statusCode != StatusCodeConstants.code200 {
                     completion(.failure(Error.invalidData))
                 } else {
-                    completion(.failure(Error.invalidData))
+                    guard let _ = try? JSONDecoder().decode(Root.self, from: data) else {
+                        completion(.failure(Error.invalidData))
+                        return
+                    }
+                    completion(.success([]))
                 }
             case .failure:
                 completion(.failure(Error.connectivity))
