7966eea73985f9c2b354b108ab77768eb2f30522
Nicolas Cadena <nicolas.cadena@globant.com>
2020-06-15 18:37:11 -0500

    Refactored the load function using the newly created FeedImagesMapper static map function

---
+++ FeedAPIChallenge/Feed API/FeedImagesMapper.swift
@@ -0,0 +1,46 @@
+//
+//  FeedImagesMapper.swift
+//  FeedAPIChallenge
+//
+//  Created by Nicolas Cadena on 15/06/20.
+//  Copyright © 2020 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
+
+class FeedImagesMapper {
+
+    // MARK: - Structs
+    private struct StatusCodeConstants {
+        static var code200 = 200
+    }
+
+    private struct Root: Decodable {
+        let items: [ImageItem]
+        
+        var feed: [FeedImage] {
+            return items.map { $0.feedImageItem }
+        }
+    }
+
+    private struct ImageItem: Decodable {
+        let image_id: UUID
+        let image_desc: String?
+        let image_loc: String?
+        let image_url: URL
+        
+        var feedImageItem: FeedImage {
+            return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+        }
+    }
+    
+    // MARK: - Functions
+    
+    static func map(data: Data, response: HTTPURLResponse) -> FeedLoader.Result {
+        guard response.statusCode == StatusCodeConstants.code200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            return .failure(RemoteFeedLoader.Error.invalidData)
+        }
+        
+        return .success(root.feed)
+    }
+}
--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -17,44 +17,12 @@ public final class RemoteFeedLoader: FeedLoader {
 		self.url = url
 		self.client = client
 	}
-    
-    // MARK: - Structs
-    private struct StatusCodeConstants {
-        static var code200 = 200
-    }
-    
-    private struct Root: Decodable {
-        let items: [Item]
-        
-        var feed: [FeedImage] {
-            return items.map { $0.item }
-        }
-    }
-
-    private struct Item: Decodable {
-        let image_id: UUID
-        let image_desc: String?
-        let image_loc: String?
-        let image_url: URL
-        
-        var item: FeedImage {
-            return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
-        }
-    }
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { (result) in
             switch result {
             case let .success((data, response)):
-                if response.statusCode != StatusCodeConstants.code200 {
-                    completion(.failure(Error.invalidData))
-                } else {
-                    guard let root = try? JSONDecoder().decode(Root.self, from: data) else {
-                        completion(.failure(Error.invalidData))
-                        return
-                    }
-                    completion(.success(root.feed))
-                }
+                completion(FeedImagesMapper.map(data: data, response: response))
             case .failure:
                 completion(.failure(Error.connectivity))
             }
