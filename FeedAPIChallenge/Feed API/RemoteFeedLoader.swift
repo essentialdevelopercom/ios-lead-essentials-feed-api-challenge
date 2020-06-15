b40b57a6fdd3dd1613d24dd8b4f65adcde7555ce
Nikita <nsdidenko.developer@gmail.com>
2020-06-15 19:27:58 +0300

    Load delivers success with items on 200 HTTP response with JSON items

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -33,10 +33,34 @@ public final class RemoteFeedLoader: FeedLoader {
     
     static func map(_ data: Data) -> FeedLoader.Result {
         do {
-            _ = try JSONSerialization.jsonObject(with: data)
-            return .success([])
+            let root = try JSONDecoder().decode(Root.self, from: data)
+            return .success(root.feed)
         } catch {
             return .failure(Error.invalidData)
         }
+    }
+}
+
+struct Root: Decodable {
+    private let items: [RemoteFeedImage]
+
+    var feed: [FeedImage] {
+        return items.map {
+            FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)
+        }
+    }
+}
+
+struct RemoteFeedImage: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let url: URL
+
+    private enum CodingKeys: String, CodingKey {
+        case id = "image_id"
+        case description = "image_desc"
+        case location = "image_loc"
+        case url = "image_url"
     }
 }
