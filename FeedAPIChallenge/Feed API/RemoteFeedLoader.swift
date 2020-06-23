c4898bf9bb5c076b6f5c78ddcddb96785e440b39
Nizzam <mohd.nizz4m@gmail.com>
2020-06-23 11:23:13 +0800

    Commit test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -7,6 +7,8 @@ import Foundation
 public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
+    
+    private let REPONSE_CODE_SUCCESS = 200
 	
 	public enum Error: Swift.Error {
 		case connectivity
@@ -19,10 +21,12 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        client.get(from: url) { (result) in
+        client.get(from: url) { [weak self] (result) in
+            guard let self = self else { return }
+            
             switch result {
-            case let .success(data, response):
-                guard response.statusCode == 200,
+            case let .success((data, response)):
+                guard response.statusCode == self.REPONSE_CODE_SUCCESS,
                     let items = try? JSONDecoder().decode(FeedImagesResponse.self, from: data) else {
                         completion(.failure(Error.invalidData))
                         return
