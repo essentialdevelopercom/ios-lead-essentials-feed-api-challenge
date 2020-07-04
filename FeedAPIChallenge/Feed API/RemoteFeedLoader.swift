13611e387d9d41a417028da5355b61d61cad03f5
Alejandro Zamudio Guajardo <alex.zamudio88@gmail.com>
2020-07-04 12:39:40 +0200

    Making the HTTP request through the `HTTPClient` property using the `URL` property to start loading the feed.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,6 +19,6 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        
+        self.client.get(from: self.url) { _ in }
     }
 }
