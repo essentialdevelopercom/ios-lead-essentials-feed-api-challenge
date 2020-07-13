9657c81a24b48507f4d5db6f169ac9cd7fe0e672
Mihai Cristescu <mihai.cristescu@gmail.com>
2020-07-13 16:48:50 +0300

    Implement delivery of invalid data error on non 200 status code HTTP response

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -5,26 +5,28 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
-	private let url: URL
-	private let client: HTTPClient
-	
-	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
-	}
-		
-	public init(url: URL, client: HTTPClient) {
-		self.url = url
-		self.client = client
-	}
-	
-	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { result in
             switch result {
                 case .failure(_):
                     completion(.failure(Error.connectivity))
-                default:
-                    break
+                case let .success((_, response)):
+                    if response.statusCode != 200 {
+                        completion(.failure(Error.invalidData))
+                }
             }
         }
     }
