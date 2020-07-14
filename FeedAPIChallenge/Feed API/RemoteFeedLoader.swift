bdeb90b70c45ac425109af9da615989e57d8bec5
Mihai Cristescu <mihai.cristescu@gmail.com>
2020-07-14 11:34:18 +0300

    Implement delivers Success With Items On 200 HTTP Response With JSON Items

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -27,19 +27,26 @@ public final class RemoteFeedLoader: FeedLoader {
                     guard response.statusCode == 200 else {
                         return completion(.failure(Error.invalidData))
                     }
-                    guard let _ = try? JSONDecoder().decode(Root.self, from: data) else {
+                    guard let remoteItems = try? JSONDecoder().decode(Root.self, from: data) else {
                         return completion(.failure(Error.invalidData))
                     }
-                    completion(.success([]))
+                    completion(.success(remoteItems.models))
             }
         }
     }
 
     private struct Root: Decodable {
         let items: [RemoteItem]
+
+        var models: [FeedImage] {
+            items.map { FeedImage(id: $0.image_id, description: $0.image_desc, location: $0.image_loc, url: $0.image_url)}
+        }
     }
 
     private struct RemoteItem: Decodable {
-        
+        let image_id: UUID
+        let image_desc: String?
+        let image_loc: String?
+        let image_url: URL
     }
 }
