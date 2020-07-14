a44f155b7817458e63d0a6515d4f6af67d6038dc
Mihai Cristescu <mihai.cristescu@gmail.com>
2020-07-14 11:46:43 +0300

    Refactoring using swift naming convention

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -46,14 +46,22 @@ public final class RemoteFeedLoader: FeedLoader {
         let items: [RemoteItem]
 
         var models: [FeedImage] {
-            items.map { FeedImage(id: $0.image_id, description: $0.image_desc, location: $0.image_loc, url: $0.image_url)}
+            items.map { FeedImage(id: $0.imageId, description: $0.imageDesc, location: $0.imageLoc, url: $0.imageUrl)}
         }
     }
 
     private struct RemoteItem: Decodable {
-        let image_id: UUID
-        let image_desc: String?
-        let image_loc: String?
-        let image_url: URL
+
+        let imageId: UUID
+        let imageDesc: String?
+        let imageLoc: String?
+        let imageUrl: URL
+
+        enum CodingKeys: String, CodingKey {
+            case imageId = "image_id"
+            case imageDesc = "image_desc"
+            case imageLoc = "image_loc"
+            case imageUrl = "image_url"
+        }
     }
 }
