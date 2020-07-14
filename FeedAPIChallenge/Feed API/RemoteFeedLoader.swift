7af50f31acf155e21a44eb9697c65d913cbdb87c
Mihai Cristescu <mihai.cristescu@gmail.com>
2020-07-14 11:15:50 +0300

    Implement delivers Invalid Data Error On 200 HTTP Response With Invalid JSON

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -7,26 +7,31 @@ import Foundation
 public final class RemoteFeedLoader: FeedLoader {
     private let url: URL
     private let client: HTTPClient
-    
+
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
     }
-    
+
     public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
-    
+
     public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { result in
             switch result {
                 case .failure(_):
                     completion(.failure(Error.connectivity))
-                case let .success((_, response)):
-                    if response.statusCode != 200 {
-                        completion(.failure(Error.invalidData))
+                case let .success((data, response)):
+                    guard response.statusCode != 200 else {
+                        return completion(.failure(Error.invalidData))
+                    }
+                    let decoder = JSONDecoder()
+                    guard let _ = try? decoder.decode([String].self, from: data) else {
+                        return completion(.failure(Error.invalidData))
                 }
+
             }
         }
     }
