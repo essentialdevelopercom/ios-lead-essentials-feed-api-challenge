ce1ad64df88ddfb47804560a9b9afb82fa8f1be6
Mauricio Figueroa <m.figueroa.olivares@live.cl>
2020-07-19 16:14:27 -0400

    Implement FeedItemsMapper to simplify the logic to serialization of the FeedItem object

---
+++ FeedAPIChallenge/Feed API/FeedItemsMapper.swift
@@ -0,0 +1,45 @@
+//
+//  FeedItemsMapper.swift
+//  FeedAPIChallenge
+//
+//  Created by Mauricio Figueroa olivares on 19-07-20.
+//  Copyright © 2020 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
+
+final class FeedItemsMapper {
+
+    private struct Root: Decodable {
+        let items: [Item]
+
+        var feed: [FeedImage] {
+            return items.map { $0.image }
+        }
+    }
+
+    private struct Item: Decodable {
+        let image_id: UUID
+        let image_desc: String?
+        let image_loc: String?
+        let image_url: URL
+
+        var image: FeedImage {
+            FeedImage(
+                id: self.image_id,
+                description: self.image_desc,
+                location: self.image_loc,
+                url: self.image_url
+            )
+        }
+    }
+
+    static func map(data: Data, response: HTTPURLResponse) -> FeedLoader.Result {
+        if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
+            return .success(root.feed)
+        }
+
+        return .failure(RemoteFeedLoader.Error.invalidData)
+    }
+}
+
