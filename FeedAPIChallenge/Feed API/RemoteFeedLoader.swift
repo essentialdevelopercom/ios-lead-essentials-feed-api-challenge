955b5a229abe9fa0630e64261d51c4c3fe40457e
Mauricio Figueroa <m.figueroa.olivares@live.cl>
2020-07-19 16:15:33 -0400

    Apply new logic of FeedItemsMapper in RemoteFeed loader when the result will be success.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,12 +21,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case let .success(data, response):
-                if response.statusCode == 200, let _ = try? JSONSerialization.jsonObject(with: data) as? [String : Any] {
-                    completion(.success([]))
-                } else {
-                    completion(.failure(Error.invalidData))
-                }
+            case let .success((data, response)):
+                completion(FeedItemsMapper.map(data: data, response: response))
             case .failure:
                 completion(.failure(Error.connectivity))
             }
