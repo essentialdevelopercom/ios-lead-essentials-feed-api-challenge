b03ddc0ed445938ac4c70bd2b4c86769fc632645
Hashem <hashemp206@yahoo.com>
2020-07-20 08:55:00 +0430

    Delivers invalid data on none 200 HTTP response

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,7 +20,13 @@ public final class RemoteFeedLoader: FeedLoader {
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { result in
-            completion(.failure(Error.connectivity))
+            switch result {
+            case let .success(data, response):
+                completion(.failure(Error.invalidData))
+            case let .failure(error):
+                completion(.failure(Error.connectivity))
+            }
+            
         }
     }
 }
