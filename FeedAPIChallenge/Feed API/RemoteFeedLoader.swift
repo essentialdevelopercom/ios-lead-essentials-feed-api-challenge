c9eb790e875df326f2ed2f8438934201a7ccc1a5
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-08-04 18:33:32 -0500

    Delivers feed images when response is 200 and JSON data has feed images.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -25,11 +25,34 @@ public final class RemoteFeedLoader: FeedLoader {
 			case .failure:
 				completion(.failure(RemoteFeedLoader.Error.connectivity))
 			case let .success((data , response)):
-				guard response.statusCode == RemoteFeedLoader.OK_200, let _ = try? JSONSerialization.jsonObject(with: data) else {
+				guard response.statusCode == RemoteFeedLoader.OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
 					return completion(.failure(RemoteFeedLoader.Error.invalidData))
 				}
-				completion(.success([]))
+				completion(.success(root.feed))
 			}
 		}
     }
+}
+
+private struct Root: Decodable {
+	private let items: [Image]
+	var feed: [FeedImage] { items.map{ $0.feedImage } }
+}
+
+private struct Image: Decodable {
+	private let id: UUID
+	private let description: String?
+	private let location: String?
+	private let url: URL
+	
+	var feedImage: FeedImage {
+		FeedImage(id: id, description: description, location: location, url: url)
+	}
+	
+	private enum CodingKeys: String, CodingKey {
+		case id = "image_id"
+		case description = "image_desc"
+		case location = "image_loc"
+		case url = "image_url"
+	}
 }
