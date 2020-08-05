69dfdd53933aa9b491b6a63efac35b4dd1fb6205
chihyin wang <a36980097@gmail.com>
2020-08-05 16:47:02 +0800

    Delivers Success with no items on 200 HTTP response with empty JSON list

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,9 +21,11 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case let .success((_, response)):
+            case let .success((data, response)):
                 if response.statusCode != 200 {
                     completion(.failure(Error.invalidData))
+                } else if let _ = try? JSONSerialization.jsonObject(with: data) {
+                    completion(.success([]))
                 } else {
                     completion(.failure(Error.invalidData))
                 }
