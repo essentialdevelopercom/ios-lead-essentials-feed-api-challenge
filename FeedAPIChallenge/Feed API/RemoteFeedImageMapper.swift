46d5f6e74d67f02214acc59d0bf285f5e75725e8
Gustavo Londono <gCorrea@buildingengines.com>
2020-08-05 09:24:48 -0500

    create a `RemoteFeedImage` model to handle the JSONDecoder to avoid indirect dependencies (Decodable) between the FeedFeature and the FeedAPI Module.

--- FeedAPIChallenge/Feed API/RemoteFeedImageMapper.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedImageMapper.swift
@@ -10,7 +10,29 @@ import Foundation
 
 struct RemoteFeedImageMapper {
     private struct Root: Decodable {
-        var items: [FeedImage]
+        private var items: [RemoteFeedImage]
+        
+        var feedImages: [FeedImage] {
+            return items.map({ $0.feedImage })
+        }
+    }
+    
+    private struct RemoteFeedImage: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let url: URL
+        
+        var feedImage: FeedImage {
+            return FeedImage(id: id, description: description, location: location, url: url)
+        }
+        
+        enum CodingKeys: String, CodingKey {
+            case id = "image_id"
+            case description = "image_desc"
+            case location = "image_loc"
+            case url = "image_url"
+        }
     }
     
     static func mapFeedImage(data: Data, httpResponse: HTTPURLResponse) -> FeedLoader.Result {
@@ -18,6 +40,6 @@ struct RemoteFeedImageMapper {
             return .failure(RemoteFeedLoader.Error.invalidData)
         }
         
-        return .success(root.items)
+        return .success(root.feedImages)
     }
 }
