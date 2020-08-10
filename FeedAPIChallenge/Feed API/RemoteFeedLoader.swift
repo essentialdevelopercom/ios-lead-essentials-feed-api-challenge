16b8ef2ff31b2ff690b1720c8cf5227a1b51d9d9
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-09 23:24:46 -0700

    Test load delivers invalid data error on 200 HTTP Response with Invalid JSON

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,9 +21,15 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case let .success((_, response)):
+            case let .success((data, response)):
                 if response.statusCode != 200 {
                     completion(.failure(Error.invalidData))
+                } else {
+                    if let _ = try? JSONDecoder().decode([FeedImage].self, from: data) {
+                        
+                    } else {
+                        completion(.failure(Error.invalidData))
+                    }
                 }
             default:
                 completion(.failure(Error.connectivity))
