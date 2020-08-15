da3fc27f91450b0cf9d7abf11a3cb6fe439a7faf
Mario Alberto Barragán Espinosa <>
2020-08-15 18:51:13 -0500

    Delivers Invalid Data Error on non 200 HTTPResponse

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,7 +20,12 @@ public final class RemoteFeedLoader: FeedLoader {
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { result in
-            if case .failure = result {
+            switch result {
+            case let .success((_, response)):
+                if response.statusCode != 200 {
+                    completion(.failure(Error.invalidData))
+                }
+            case .failure(_):
                 completion(.failure(Error.connectivity))
             }
         }
