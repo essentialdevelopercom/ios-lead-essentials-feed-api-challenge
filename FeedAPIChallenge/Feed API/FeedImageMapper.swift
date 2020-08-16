9ef4e3caf0f7601315e07f2a0fa9d6e8e8ec4cfe
Mario Alberto Barragán Espinosa <>
2020-08-16 12:50:10 -0500

    Move FeedImageMapper to its own file

---
+++ FeedAPIChallenge/Feed API/FeedImageMapper.swift
@@ -0,0 +1,37 @@
+//
+//  FeedImageMapper.swift
+//  FeedAPIChallenge
+//
+//  Created by Mario Alberto Barragán Espinosa on 16/08/20.
+//  Copyright © 2020 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
+
+internal class FeedImageMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+        
+        var feed: [FeedImage] {
+            return items.map({ $0.item })
+        }
+    }
+
+    private struct Item: Decodable {
+        let image_id: UUID
+        let image_desc: String?
+        let image_loc: String?
+        let image_url: URL
+        
+        var item: FeedImage {
+            return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+        }
+    }
+    
+    internal static func map(_ data: Data, response: HTTPURLResponse) -> FeedLoader.Result {
+        guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            return .failure(RemoteFeedLoader.Error.invalidData)
+        }
+        return .success(root.feed)
+    }
+}
--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -31,31 +31,3 @@ public final class RemoteFeedLoader: FeedLoader {
         }
     }
 }
-
-private class FeedImageMapper {
-    static func map(_ data: Data, response: HTTPURLResponse) -> FeedLoader.Result {
-        guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return .failure(RemoteFeedLoader.Error.invalidData)
-        }
-        return .success(root.feed)
-    }
-}
-
-private struct Root: Decodable {
-    let items: [Item]
-    
-    var feed: [FeedImage] {
-        return items.map({ $0.item })
-    }
-}
-
-private struct Item: Decodable {
-    let image_id: UUID
-    let image_desc: String?
-    let image_loc: String?
-    let image_url: URL
-    
-    var item: FeedImage {
-        return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
-    }
-}
