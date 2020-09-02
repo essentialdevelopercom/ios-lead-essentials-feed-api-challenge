b66eebdcb6113213b49d880b5c56d4ea5e5b0314
Eric Sans <cocoataster@protonmail.ch>
2020-09-02 17:16:25 +0100

    Add FeedItemsMapper

--- FeedAPIChallenge/Feed Feature/FeedImage.swift
+++ FeedAPIChallenge/Feed Feature/FeedImage.swift
@@ -19,3 +19,17 @@ public struct FeedImage: Hashable {
 }
 
 extension FeedImage: Decodable {}
+
+final class FeedItemsMapper {
+    private struct Root: Decodable {
+        let items: [FeedImage]
+    }
+    
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [FeedImage] {
+        guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        
+        return root.items
+    }
+}
