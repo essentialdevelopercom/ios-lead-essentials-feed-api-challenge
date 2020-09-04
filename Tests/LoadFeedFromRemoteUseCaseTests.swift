7d6a60ae9a04b0da69b2fecf995351a141d06e16
Christophe Bugnon <christophe.bugnon@musicworldmedia.com>
2020-09-04 10:28:42 +0200

    Load request data twice from url

--- Tests/LoadFeedFromRemoteUseCaseTests.swift
+++ Tests/LoadFeedFromRemoteUseCaseTests.swift
@@ -21,9 +21,11 @@ class RemoteFeedLoader {
 
 class HTTPClient {
     var requestedURL: URL?
+    var requestedURLs: [URL] = []
     
     func get(from url: URL) {
         requestedURL = url
+        requestedURLs.append(url)
     }
 }
 
@@ -55,6 +57,16 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    func test_loadTwice_requestDataFromURLTwice() {
+        let url = URL(string: "another-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
     // MARK: - Helpers
