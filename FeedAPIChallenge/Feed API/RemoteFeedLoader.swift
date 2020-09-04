398ea37748128d6fe873b5bf63e8359a92c1826a
Christophe Bugnon <christophe.bugnon@musicworldmedia.com>
2020-09-04 07:04:43 +0200

    Clean project

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -5,17 +5,11 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
-	private let url: URL
-	private let client: HTTPClient
 	
 	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
 	}
 		
-	public init(url: URL, client: HTTPClient) {
-		self.url = url
-		self.client = client
+	public init() {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
