1b2eb8bcee33cbe2fe35f0cdefe602c25871d1e9
Araceli Ruiz Ruiz <araruizalcuadrado@gmail.com>
2020-09-06 15:08:28 +0200

    test_load_deliversSuccessWithItemsOn200HTTPResponseWithJSONItems

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -25,7 +25,11 @@ public final class RemoteFeedLoader: FeedLoader {
                 if response.statusCode != 200 {
                     completion(.failure(Error.invalidData))
                 } else if (try? JSONSerialization.jsonObject(with: data)) != nil {
-                    completion(.success([]))
+                    if let root = try? JSONDecoder().decode(Root.self, from: data) {
+                        completion(.success(root.items.toModel()))
+                    } else {
+                        completion(.success([]))
+                    }
                 } else {
                     completion(.failure(Error.invalidData))
                 }
@@ -33,5 +37,22 @@ public final class RemoteFeedLoader: FeedLoader {
                 completion(.failure(Error.connectivity))
             }
         })
+    }
+}
+
+private struct Root: Decodable {
+    let items: [RemoteFeedImage]
+}
+
+private struct RemoteFeedImage: Decodable {
+    let image_id: UUID
+    let image_desc: String?
+    let image_loc: String?
+    let image_url: URL
+}
+
+extension Array where Element == RemoteFeedImage {
+    func toModel() -> [FeedImage] {
+        return map { FeedImage(id: $0.image_id, description: $0.image_desc, location: $0.image_loc, url: $0.image_url) }
     }
 }
