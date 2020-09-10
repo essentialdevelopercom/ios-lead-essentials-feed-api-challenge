d206619947c34f4874f8bc976523a0cd5a4978b5
“Shady <shady.kahaleh@gmail.com>
2020-09-11 00:28:46 +0300

    test load twice url

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -7,18 +7,19 @@ import Foundation
 public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
+    private var urls = [URL]()
 	
 	public enum Error: Swift.Error {
 		case connectivity
 		case invalidData
 	}
 		
-	public init(url: URL, client: HTTPClient) {
+    public init(url: URL, client: HTTPClient) {
 		self.url = url
 		self.client = client
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        
+        client.get(from: url){_ in }
     }
 }
