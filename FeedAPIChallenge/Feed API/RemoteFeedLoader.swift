4cd4002c6667f50c767f1f25ed2d13a53d5de9df
“Shady <shady.kahaleh@gmail.com>
2020-09-11 01:22:36 +0300

    make sure not to complete when RemoteFeedLoader is deallocated

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,7 +21,9 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        client.get(from: url){result in
+        client.get(from: url){[weak self] result in
+            guard self != nil else {return}
+            
             switch result{
             case  .failure(_):
                 completion(.failure(RemoteFeedLoader.Error.connectivity))
