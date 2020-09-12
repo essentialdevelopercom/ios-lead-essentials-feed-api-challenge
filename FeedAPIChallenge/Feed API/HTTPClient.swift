3dc3625f10e827eecaeb6441565aabc798a31b97
aaronmhr <aaronmhr@gmail.com>
2020-09-12 22:19:07 +0200

    Improve readability by giving names to tuple elements

--- FeedAPIChallenge/Feed API/HTTPClient.swift
+++ FeedAPIChallenge/Feed API/HTTPClient.swift
@@ -5,7 +5,7 @@
 import Foundation
 
 public protocol HTTPClient {
-	typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    typealias Result = Swift.Result<(data: Data, response: HTTPURLResponse), Error>
 	
 	func get(from url: URL, completion: @escaping (Result) -> Void)
 }
