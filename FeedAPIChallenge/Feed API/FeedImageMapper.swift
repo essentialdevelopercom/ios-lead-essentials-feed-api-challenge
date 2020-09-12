aa80f29332a1eb68d9586b75657e52b48caac55a
aaronmhr <aaronmhr@gmail.com>
2020-09-12 22:48:53 +0200

    Mode `FeedImageMapper` to its own file

---
+++ FeedAPIChallenge/Feed API/FeedImageMapper.swift
@@ -0,0 +1,38 @@
+//
+//  FeedImageMapper.swift
+//  FeedAPIChallenge
+//
+//  Created by Aaron Huánuco on 12/09/2020.
+//  Copyright © 2020 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
+
+final class FeedImageMapper {
+    private typealias Error = RemoteFeedLoader.Error
+    private static var OK_STATUS: Int { 200 }
+
+    static func map(_ data: Data, _ response: HTTPURLResponse) -> FeedLoader.Result {
+        if response.statusCode == OK_STATUS,
+            let feed = try? JSONDecoder().decode(Root.self, from: data) {
+            return .success(feed.items.map(\.feedImage))
+        } else {
+            return .failure(Error.invalidData)
+        }
+    }
+
+    private struct Root: Decodable {
+        let items: [FeedImageResponse]
+    }
+
+    private struct FeedImageResponse: Decodable {
+        private let image_id: UUID
+        private let image_desc: String?
+        private let image_loc: String?
+        private let image_url: URL
+
+        var feedImage: FeedImage {
+            FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+        }
+    }
+}
--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -30,31 +30,3 @@ public final class RemoteFeedLoader: FeedLoader {
         }
     }
 }
-
-final class FeedImageMapper {
-    typealias Error = RemoteFeedLoader.Error
-
-    static func map(_ data: Data, _ response: HTTPURLResponse) -> FeedLoader.Result {
-        if response.statusCode == 200,
-            let feed = try? JSONDecoder().decode(Root.self, from: data) {
-            return .success(feed.items.map(\.feedImage))
-        } else {
-            return .failure(Error.invalidData)
-        }
-    }
-
-    private struct Root: Decodable {
-        let items: [FeedImageResponse]
-    }
-
-    struct FeedImageResponse: Decodable {
-        private let image_id: UUID
-        private let image_desc: String?
-        private let image_loc: String?
-        private let image_url: URL
-
-        var feedImage: FeedImage {
-            FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
-        }
-    }
-}
