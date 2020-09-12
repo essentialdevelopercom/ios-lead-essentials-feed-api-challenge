0410e895f5e5553eccd2592273ca3f39671944d9
Raphael Silva <sr.silvaraphael@gmail.com>
2020-09-12 16:10:46 +0200

    Create FeedMapper to handle mapping logic

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,12 +22,7 @@ public final class RemoteFeedLoader: FeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success((data, response)):
-                guard
-                    response.statusCode == 200,
-                    let root = try? JSONDecoder().decode(Root.self, from: data) else {
-                        return completion(.failure(Error.invalidData))
-                }
-                completion(.success(root.feed))
+                completion(FeedMapper.map(data, response: response))
             case .failure:
                 completion(.failure(Error.connectivity))
             }
@@ -35,31 +30,43 @@ public final class RemoteFeedLoader: FeedLoader {
     }
 }
 
-private struct Root: Decodable {
-    private let items: [RemoteFeedImage]
+final class FeedMapper {
+    private struct Root: Decodable {
+        private let items: [RemoteFeedImage]
 
-    var feed: [FeedImage] {
-        return items.map {
-            return FeedImage(
-                id: $0.id,
-                description: $0.description,
-                location: $0.location,
-                url: $0.url
-            )
+        var feed: [FeedImage] {
+            return items.map {
+                return FeedImage(
+                    id: $0.id,
+                    description: $0.description,
+                    location: $0.location,
+                    url: $0.url
+                )
+            }
         }
     }
-}
 
-private struct RemoteFeedImage: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let url: URL
+    private struct RemoteFeedImage: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let url: URL
 
-    enum CodingKeys: String, CodingKey {
-        case id = "image_id"
-        case description = "image_desc"
-        case location = "image_loc"
-        case url = "image_url"
+        enum CodingKeys: String, CodingKey {
+            case id = "image_id"
+            case description = "image_desc"
+            case location = "image_loc"
+            case url = "image_url"
+        }
+    }
+
+    static func map(_ data: Data, response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+        guard
+            response.statusCode == 200,
+            let root = try? JSONDecoder().decode(Root.self, from: data) else {
+                return .failure(RemoteFeedLoader.Error.invalidData)
+        }
+
+        return .success(root.feed)
     }
 }
