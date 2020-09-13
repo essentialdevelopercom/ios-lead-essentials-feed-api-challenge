5bc318340ec658d657450b51ad2f883cdd988e68
GeorgeLIu <g5321247@gmail.com>
2020-09-13 11:42:54 +0800

    Delivered invalid data error with invalid JSON data on 200 HTTP URLResponse

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,8 +21,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { (result) in
             switch result {
-            case .success((_, let response)):
-                if response.statusCode == 200 {
+            case .success(let (data, response)):
+                if response.statusCode == 200, let item = try? JSONDecoder().decode(Root.self, from: data) {
                     break
                 } else {
                     completion(.failure(RemoteFeedLoader.Error.invalidData))
@@ -30,6 +30,36 @@ public final class RemoteFeedLoader: FeedLoader {
             case .failure(_):
                 completion(.failure(RemoteFeedLoader.Error.connectivity))
             }
+        }
+    }
+}
+
+private struct Root: Decodable {
+    private let items: [Image]
+    var feedImages: [FeedImage] {
+        return items.map { $0.feedImage }
+    }
+
+    struct Image: Decodable {
+        let id: UUID
+        let description: String
+        let location: String
+        let url: URL
+
+        var feedImage: FeedImage {
+            return FeedImage(
+                id: id,
+                description: description,
+                location: location,
+                url: url
+            )
+        }
+
+        enum CodingKeys: String, CodingKey {
+            case id = "image_id"
+            case description = "image_desc"
+            case location = "image_loc"
+            case url = "image_url"
         }
     }
 }
