9a633d3c0eaf6de490edf4676b03aff628613250
GeorgeLIu <g5321247@gmail.com>
2020-09-13 18:06:42 +0800

    Move `FeedIamgeMapper` to the file

---
+++ FeedAPIChallenge/Feed API/FeedIamgeMapper.swift
@@ -0,0 +1,49 @@
+//
+//  FeedIamgeMapper.swift
+//  FeedAPIChallenge
+//
+//  Created by George Liu on 2020/9/13.
+//  Copyright © 2020 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
+
+final class FeedIamgeMapper {
+    static func map(data: Data, response: HTTPURLResponse) -> FeedLoader.Result {
+        guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            return .failure(RemoteFeedLoader.Error.invalidData)
+        }
+
+        return .success(root.feedImages)
+    }
+
+    struct Root: Decodable {
+        private let items: [Image]
+        var feedImages: [FeedImage] {
+            return items.map { $0.feedImage }
+        }
+
+        struct Image: Decodable {
+            let id: UUID
+            let description: String?
+            let location: String?
+            let url: URL
+
+            var feedImage: FeedImage {
+                return FeedImage(
+                    id: id,
+                    description: description,
+                    location: location,
+                    url: url
+                )
+            }
+
+            enum CodingKeys: String, CodingKey {
+                case id = "image_id"
+                case description = "image_desc"
+                case location = "image_loc"
+                case url = "image_url"
+            }
+        }
+    }
+}
