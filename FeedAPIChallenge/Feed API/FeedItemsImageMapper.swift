d2714144988491521037878f62075593c63ca190
Sami <sami79031@yahoo.com>
2020-10-03 15:30:05 +0300

    Refactoring

---
+++ FeedAPIChallenge/Feed API/FeedItemsImageMapper.swift
@@ -0,0 +1,44 @@
+//
+//  FeedItemsImageMapper.swift
+//  FeedAPIChallenge
+//
+//  Created by Sami Ali on 10/3/20.
+//  Copyright © 2020 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
+
+internal final class FeedItemsImageMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+        
+        var feed: [FeedImage] {
+            return items.map { $0.item }
+        }
+    }
+
+    private struct Item: Decodable {
+        let image_id: UUID
+        let image_desc: String?
+        let image_loc: String?
+        let image_url: URL
+
+        var item: FeedImage {
+            return FeedImage(id: image_id,
+                             description: image_desc,
+                             location: image_loc,
+                             url: image_url)
+        }
+    }
+
+    private static var OK_200: Int { return 200 }
+    
+    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+        guard response.statusCode == OK_200,
+              let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            return .failure(RemoteFeedLoader.Error.invalidData)
+        }
+
+        return .success(root.feed)
+    }
+}
