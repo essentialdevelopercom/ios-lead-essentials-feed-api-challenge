c53e8b08061f3135c32a870cb524593a05f680c0
Maxim Soldatov <maxim.soldatov@me.com>
2020-10-04 17:30:48 +0300

    Added single approach for handling two kinds of custom errors

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -5,26 +5,26 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
-	private let url: URL
-	private let client: HTTPClient
-	
-	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
-	}
-		
-	public init(url: URL, client: HTTPClient) {
-		self.url = url
-		self.client = client
-	}
-	
-	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+    private let url: URL
+    private let client: HTTPClient
+    
+    private static var OK_200: Int { return 200 }
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url, completion: { result in
             switch result {
-            case let .success((_, response)):
-            if response.statusCode != 200 {
+            case .success(_):
                 completion(.failure(Error.invalidData))
-            } 
             case .failure(_):
                 completion(.failure(Error.connectivity))
             }
