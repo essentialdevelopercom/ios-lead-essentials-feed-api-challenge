c6ee21a77f15ad640ad3f44336d97f2b4554836d
Maxim Soldatov <maxim.soldatov@me.com>
2020-10-04 16:38:44 +0300

    Added method invoke 'get' in RemoteFeedLoader 'load' function

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,6 +19,7 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        
+        client.get(from: url, completion: {_ in })
     }
 }
+Added method call get in
