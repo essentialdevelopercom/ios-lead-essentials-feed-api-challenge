a730c880737d645fa2ad569c3fb41b99a2e53425
Andres Rivas <andres.rivas@rappi.com>
2020-10-04 16:17:39 -0300

    Added RemoteFeedImage parsing struct to interpret backend json response and generate a valid FeedImage

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -23,14 +23,45 @@ public final class RemoteFeedLoader: FeedLoader {
             switch result {
             case let .success((data, response)):
                 guard response.statusCode == 200,
-                      let _ = try? JSONSerialization.jsonObject(with: data) else {
+                      let items = try? JSONDecoder().decode(RemoteFeedImageRoot.self, from: data).feedItems else {
                     completion(.failure(Error.invalidData))
                     return
                 }
-                completion(.success([]))
+                completion(.success(items))
             case .failure:
                 completion(.failure(Error.connectivity))
             }
         }
     }
+}
+
+struct RemoteFeedImageRoot: Decodable {
+    
+    var items: [RemoteFeedImage]
+    
+    var feedItems: [FeedImage] {
+        items.map { $0.feedImage }
+    }
+}
+
+struct RemoteFeedImage: Decodable {
+    var id: UUID
+    var description: String?
+    var location: String?
+    var url: URL
+    
+    var feedImage: FeedImage {
+        return FeedImage(id: id,
+                         description: description,
+                         location: location,
+                         url: url)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "image_id"
+        case description = "image_desc"
+        case location = "image_loc"
+        case url = "image_url"
+    }
+    
 }
