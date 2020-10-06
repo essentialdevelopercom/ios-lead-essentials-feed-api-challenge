ed715ccf84063cd848d2a69fe0e8dd73d0dbbe2f
Lukas Bahrle <lukas.bahrle.santana@gmail.com>
2020-10-06 16:53:03 +0200

    Return a connectivity error on client failure case

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,8 +19,13 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        client.get(from: url){ _ in
-            
+        client.get(from: url){ result in
+            switch(result){
+            case .failure(_):
+                completion(.failure(RemoteFeedLoader.Error.connectivity))
+            default:
+                break
+            }
         }
     }
 }
