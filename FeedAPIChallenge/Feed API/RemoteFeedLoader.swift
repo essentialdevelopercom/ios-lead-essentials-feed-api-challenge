e0e427ba4ced1912bf1a5c7982a93271f516c43b
Erich Flock <erich.flock@check24.de>
2020-10-06 16:08:03 +0200

    Load function should request data from url with given client

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,5 +20,11 @@ public final class RemoteFeedLoader: FeedLoader {
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         
+        client.get(from: url, completion: { result in
+            switch result {
+            default:
+                break
+            }
+        })
     }
 }
