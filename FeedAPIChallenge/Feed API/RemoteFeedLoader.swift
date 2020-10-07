0cbfd53a78b6b37d127b352f6336b6cb3f2a7cda
Lukas Bahrle <lukas.bahrle.santana@gmail.com>
2020-10-07 15:25:27 +0200

    Add a Mapper to decode the feed images response data

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -29,16 +29,50 @@ public final class RemoteFeedLoader: FeedLoader {
                     return
                 }
                 
-                do{
-                    try JSONSerialization.jsonObject(with: data)
-                    
-                    completion(.success([]))
-                }
-                catch{
-                    completion(.failure(RemoteFeedLoader.Error.invalidData))
-                }
-                
+                completion(FeedImagesMapper.map(data))
             }
         }
     }
 }
+
+
+
+private struct FeedImagesMapper: Codable{
+    private struct Root: Decodable{
+        let items: [Item]
+        
+        var feedImages: [FeedImage] {
+                return items.map { $0.item }
+            }
+    }
+    
+    private struct Item: Decodable{
+        let id: UUID
+        let description: String?
+        let location: String?
+        let url: URL
+        
+        var item: FeedImage {
+            FeedImage(id: id, description: description, location: location, url: url)
+        }
+        
+        private enum CodingKeys: String, CodingKey{
+            case id = "image_id"
+            case description = "image_desc"
+            case location = "image_loc"
+            case url = "image_url"
+        }
+    }
+    
+    internal static func map(_ data: Data) -> FeedLoader.Result{
+        do{
+            let root = try JSONDecoder().decode(Root.self, from: data)
+            return .success(root.feedImages)
+        }
+        catch{
+            return .failure(RemoteFeedLoader.Error.invalidData)
+        }
+    }
+}
+
+
