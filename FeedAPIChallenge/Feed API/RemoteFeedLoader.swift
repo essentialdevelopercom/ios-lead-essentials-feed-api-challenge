4e7ec99566f7d106b144e109261bca6a8d3e93c0
GeorgeSolorio <Georgemsolorio@gmail.com>
2020-10-07 17:11:52 -0700

    Handle invalid JSON data on 200 HTTP response with invalid data error

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,8 +21,12 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
       client.get(from: url) { result in
          switch result {
-         case .success:
-            completion(.failure(Error.invalidData))
+         case let .success((_, reponse)):
+            if reponse.statusCode == 200 {
+               completion(.failure(Error.invalidData))
+            } else {
+               completion(.failure(Error.invalidData))
+            }
          case .failure:
             completion(.failure(Error.connectivity))
          }
