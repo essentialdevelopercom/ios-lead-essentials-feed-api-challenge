322e0a8fee6daded3500fc7b093dc77562bc20d6
GeorgeSolorio <Georgemsolorio@gmail.com>
2020-10-07 18:00:55 -0700

    Delivers empty array of images on 200 HTTP response with JSON empty list

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,9 +21,10 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
       client.get(from: url) { result in
          switch result {
-         case let .success((_, reponse)):
-            if reponse.statusCode == 200 {
-               completion(.failure(Error.invalidData))
+         case let .success((data, response)):
+            if let _ = try? JSONSerialization.jsonObject(with: data),
+               response.statusCode == 200 {
+                  completion(.success([]))
             } else {
                completion(.failure(Error.invalidData))
             }
