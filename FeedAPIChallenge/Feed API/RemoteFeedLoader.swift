2f8175a8cc445e7389e42da3c5e64d36175f36a6
GeorgeSolorio <Georgemsolorio@gmail.com>
2020-10-07 19:59:55 -0700

    Replace hard coded value with extra context

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -58,9 +58,11 @@ private class FeedImagesMapper {
       }
    }
    
+   static var OK_200 = 200
+   
    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
       
-      guard response.statusCode == 200 else {
+      guard response.statusCode == OK_200 else {
          throw RemoteFeedLoader.Error.invalidData
       }
       
