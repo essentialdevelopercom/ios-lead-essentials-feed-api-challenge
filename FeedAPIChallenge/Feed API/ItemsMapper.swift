df913c5d0575d2bcf2797b3cb03ace966387ee7b
Erich Flock <erich.flock@check24.de>
2020-10-09 13:37:21 +0200

    Move ItemsMapper class to its own file

---
+++ FeedAPIChallenge/Feed API/ItemsMapper.swift
@@ -0,0 +1,44 @@
+//
+//  ItemsMapper.swift
+//  FeedAPIChallenge
+//
+//  Created by Erich Flock on 09.10.20.
+//  Copyright © 2020 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
+
+class ItemsMapper {
+    
+    private struct Root: Decodable {
+        
+        let items: [Item]
+        
+        var feed: [FeedImage] {
+            return items.map { $0.item }
+        }
+    }
+
+    private struct Item: Decodable {
+        
+        let image_id: UUID
+        let image_desc: String?
+        let image_loc: String?
+        let image_url: URL
+
+        var item: FeedImage {
+            return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+        }
+    }
+    
+    func mapJsonResponse(jsonResponse: Data, completion: @escaping ([FeedImage]) -> Void) {
+        
+        do {
+            let root = try JSONDecoder().decode(ItemsMapper.Root.self, from: jsonResponse)
+            completion(root.feed)
+        } catch {
+            completion([])
+        }
+        
+    }
+}
--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -51,38 +51,3 @@ public final class RemoteFeedLoader: FeedLoader {
     }
         
 }
-
-class ItemsMapper {
-    
-    private struct Root: Decodable {
-        
-        let items: [Item]
-        
-        var feed: [FeedImage] {
-            return items.map { $0.item }
-        }
-    }
-
-    private struct Item: Decodable {
-        
-        let image_id: UUID
-        let image_desc: String?
-        let image_loc: String?
-        let image_url: URL
-
-        var item: FeedImage {
-            return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
-        }
-    }
-    
-    func mapJsonResponse(jsonResponse: Data, completion: @escaping ([FeedImage]) -> Void) {
-        
-        do {
-            let root = try JSONDecoder().decode(ItemsMapper.Root.self, from: jsonResponse)
-            completion(root.feed)
-        } catch {
-            completion([])
-        }
-        
-    }
-}
