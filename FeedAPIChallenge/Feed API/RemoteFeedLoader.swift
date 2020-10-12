21615a0aec40c0fb3a36be440ab458ab18644fa1
Dan Hung <dan12411@gmail.com>
2020-10-12 15:58:28 +0800

    fix test_load_deliversConnectivityErrorOnClientError() by giving specific .connectivity Error to completion failure result.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -23,8 +23,8 @@ public final class RemoteFeedLoader: FeedLoader {
             switch result {
             case .success:
                 completion(.failure(Error.invalidData))
-            case let .failure(error):
-                completion(.failure(error))
+            case .failure:
+                completion(.failure(Error.connectivity))
             }
         }
     }
