dae15f519328fd61ecf3f257765605c759a1aec3
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2020-10-16 11:35:34 +0800

    Delivers success with items on 200HTTPResponse with JSON items

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,8 +22,8 @@ public final class RemoteFeedLoader: FeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success((data, response)):
-                if let _ = try? JSONSerialization.jsonObject(with: data, options: []), response.statusCode == 200 {
-                    completion(.success([]))
+                if let root = try? JSONDecoder().decode(Root.self, from: data), response.statusCode == 200 {
+                    completion(.success(root.items.map { $0.feedImage}))
                 } else {
                     completion(.failure(Error.invalidData))
                 }
@@ -31,7 +31,33 @@ public final class RemoteFeedLoader: FeedLoader {
                 completion(.failure(Error.connectivity))
     
             }
-            
         }
+    }
+}
+
+
+struct Root: Decodable {
+    let items: [RemoteFeedImage]
+}
+
+struct RemoteFeedImage: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let url: URL
+
+    enum CodingKeys: String, CodingKey {
+        case id = "image_id"
+        case description = "image_desc"
+        case location = "image_loc"
+        case url = "image_url"
+    }
+    
+    var feedImage: FeedImage {
+        return FeedImage(
+            id: id,
+            description: description,
+            location: location,
+            url: url)
     }
 }
