12bb435c4f7beed108720514c89b28df83d88901
Ángel Vázquez <angel_betta@outlook.com>
2020-10-18 17:35:01 -0500

    Ensure invalidData error is delivered for non-200 HTTP response

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,8 +21,10 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case .success:
+            case .success((_, let response)) where response.statusCode != 200:
                 completion(.failure(Error.invalidData))
+            case .success:
+                break
             case .failure:
                 completion(.failure(Error.connectivity))
             }
