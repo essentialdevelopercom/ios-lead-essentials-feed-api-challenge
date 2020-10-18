d0a97f7653ec2902259e5b084d25b6f6e1ae9d2f
Ángel Vázquez <angel_betta@outlook.com>
2020-10-18 17:39:39 -0500

    Refactor code to use only one case in switch statement

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,11 +21,11 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case .success((_, let response)) where response.statusCode != 200:
+            case let .success((_, response)):
+                guard response.statusCode == 200 else {
+                    return completion(.failure(Error.invalidData))
+                }
                 completion(.failure(Error.invalidData))
-            case .success:
-                completion(.failure(Error.invalidData))
-                break
             case .failure:
                 completion(.failure(Error.connectivity))
             }
