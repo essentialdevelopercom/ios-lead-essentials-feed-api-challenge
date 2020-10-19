a75492f37fdd81ccfd90be5d3992efcc97796e47
Rakesh Ramamurthy <rramamur@visa.com>
2020-10-19 20:48:36 +0530

    Refactored Result.success block by introducing FeedImageMapper

---
+++ FeedAPIChallenge/Feed API/FeedImageMapper.swift
@@ -0,0 +1,41 @@
+//
+//  FeedImageMapper.swift
+//  FeedAPIChallenge
+//
+//  Created by Rakesh Ramamurthy on 19/10/20.
+//  Copyright © 2020 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
+
+internal final class FeedImageMapper {
+    private struct Root: Decodable {
+        let images: [ImageItem]
+        
+        var feed: [FeedImage] {
+            return images.map { $0.image }
+        }
+    }
+
+    private struct ImageItem: Decodable {
+        let image_id: UUID
+        let image_desc: String?
+        let image_loc: String?
+        let image_url: URL
+        
+        var image: FeedImage {
+            return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+        }
+    }
+
+    private static var OK_HTTP_200: Int { return 200 }
+    
+    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+        guard response.statusCode == OK_HTTP_200,
+        let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            return .failure(RemoteFeedLoader.Error.invalidData)
+        }
+
+        return .success(root.feed)
+    }
+}
--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,12 +22,7 @@ public final class RemoteFeedLoader: FeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success((data, response)):
-                if response.statusCode == 200,
-                    let _ = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success([]))
-                } else {
-                    completion(.failure(Error.invalidData))
-                }
+                completion(FeedImageMapper.map(data, from: response))
             case .failure:
                 completion(.failure(Error.connectivity))
             }
