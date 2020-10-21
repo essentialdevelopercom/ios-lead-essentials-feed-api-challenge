11a19f1c5804fe3c41540e229880074a603a749e
Vishal Deep Kanojia <vishal.deep@live.com>
2020-10-21 13:15:56 +0530

    RemoteFeedLoader responds with empty array on empty JSON list HTTP response

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,8 +21,18 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { (result) in
             switch result {
-            case .success:
-                completion(.failure(Error.invalidData))
+            case let .success((data, response)):
+                guard response.statusCode == 200 else {
+                    completion(.failure(Error.invalidData))
+                    return
+                }
+
+                guard let _ = try? JSONSerialization.jsonObject(with: data, options: [.allowFragments]) else {
+                    completion(.failure(Error.invalidData))
+                    return
+                }
+
+                completion(.success([]))
             case .failure:
                 completion(.failure(Error.connectivity))
             }
