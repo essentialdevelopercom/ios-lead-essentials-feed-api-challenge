da9e7afef9f76bce601de9ebb0ab3eae3d717546
Valentin Šarić <valentin.saric@analyticom.de>
2020-10-23 10:57:27 +0200

    Add parsing models, ´RemoteFeedLoader´ delivers success with items on 200 HTTPResponse with JSON items

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -5,50 +5,56 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
-	private let url: URL
-	private let client: HTTPClient
-	
-	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
-	}
+    private let url: URL
+    private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
     
     private struct Root: Codable{
         let items: [FeedImageParseModel]
     }
     
     private struct FeedImageParseModel: Codable {
-        public let id: UUID
-        public let description: String?
-        public let location: String?
-        public let url: URL
+        public let imageId: UUID
+        public let imageDesc: String?
+        public let imageLoc: String?
+        public let imageUrl: URL
         
         public init(id: UUID, description: String?, location: String?, url: URL) {
-            self.id = id
-            self.description = description
-            self.location = location
-            self.url = url
+            self.imageId = id
+            self.imageDesc = description
+            self.imageLoc = location
+            self.imageUrl = url
         }
     }
-		
-	public init(url: URL, client: HTTPClient) {
-		self.url = url
-		self.client = client
-	}
-	
-	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url, completion: { result in
             switch result{
             case .failure(_):
                 completion(.failure(Error.connectivity))
             case let .success( (data, response)):
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
                 guard response.statusCode == 200,
-                      let _ = try? JSONDecoder().decode(Root.self, from: data)
+                      let data = try? decoder.decode(Root.self, from: data)
                 else{
                     completion(.failure(Error.invalidData))
                     return
                 }
-                completion(.success(.init()))
+                completion(.success(
+                    data.items.map({
+                        FeedImage.init(id: $0.imageId, description: $0.imageDesc, location: $0.imageLoc, url: $0.imageUrl)
+                    })
+                ))
             }
         })
     }
