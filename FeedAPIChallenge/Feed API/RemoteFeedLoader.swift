01d550069c58250ff2ba504e13e0866691880390
Juan López <>
2020-10-25 13:06:32 +0100

    Guarantee delivering of success with items on 200 HTTP response with json items

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,8 +22,8 @@ public final class RemoteFeedLoader: FeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success((data, response)):
-                if response.statusCode == 200, (try? JSONSerialization.jsonObject(with: data)) != nil {
-                    completion(.success([]))
+                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
+                    completion(.success(root.items))
                 } else {
                     completion(.failure(Error.invalidData))
                 }
@@ -32,4 +32,8 @@ public final class RemoteFeedLoader: FeedLoader {
             }
         }
     }
+}
+
+private struct Root: Codable {
+    let items: [FeedImage]
 }
