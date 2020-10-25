445513aa442aaf77df108776a650ea9afa3a2351
Juan López <>
2020-10-25 13:38:12 +0100

    Remove coments

--- Tests/LoadFeedFromRemoteUseCaseTests.swift
+++ Tests/LoadFeedFromRemoteUseCaseTests.swift
@@ -6,18 +6,6 @@ import XCTest
 import FeedAPIChallenge
 
 class LoadFeedFromRemoteUseCaseTests: XCTestCase {
-	
-    //  ***********************
-    //
-    //  Follow the TDD process:
-    //
-    //  1. Uncomment and run one test at a time (run tests with CMD+U).
-    //  2. Do the minimum to make the test pass and commit.
-    //  3. Refactor if needed and commit again.
-    //
-    //  Repeat this process until all tests are passing.
-    //
-    //  ***********************
     
 	func test_init_doesNotRequestDataFromURL() {
 		let (_, client) = makeSUT()
