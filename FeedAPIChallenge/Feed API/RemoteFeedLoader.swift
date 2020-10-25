e9a07051b5f7f34290bb1f08d2cce48c2fef13f6
Daniel Munteanu <daniel.munteanu@cgm.com>
2020-10-25 14:11:42 +0200

    Made `test_load_deliversInvalidDataErrorOn200HTTPResponseWithInvalidJSON` pass by completing with `Error.invalidData` when a valid JSON object cannot be created from data received when HTTPClient completes

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,8 +21,10 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case .success((_, let response)):
+            case let .success((data, response)):
                 if response.statusCode != 200 {
+                    completion(.failure(Error.invalidData))
+                } else if (try? JSONSerialization.jsonObject(with: data)) == nil {
                     completion(.failure(Error.invalidData))
                 }
             case .failure:
