33eb7768a4fe89bbcc607c0caff9ebf6667851eb
Daniel Munteanu <daniel.munteanu@cgm.com>
2020-10-25 14:21:59 +0200

    Made `test_load_deliversSuccessWithNoItemsOn200HTTPResponseWithEmptyJSONList` pass by completing with success empty array when no items are found in the data received from HTTPClient. Refactored if else into guard let approach for better visibility.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,10 +22,16 @@ public final class RemoteFeedLoader: FeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success((data, response)):
-                if response.statusCode != 200 {
+                guard response.statusCode == 200 else {
                     completion(.failure(Error.invalidData))
-                } else if (try? JSONSerialization.jsonObject(with: data)) == nil {
+                    return
+                }
+                guard let jsonDictionary = try? JSONSerialization.jsonObject(with: data) as? [String: Any], let items = jsonDictionary["items"] as? [Any] else {
                     completion(.failure(Error.invalidData))
+                    return
+                }
+                if items.isEmpty {
+                    completion(.success([]))
                 }
             case .failure:
                 completion(.failure(Error.connectivity))
