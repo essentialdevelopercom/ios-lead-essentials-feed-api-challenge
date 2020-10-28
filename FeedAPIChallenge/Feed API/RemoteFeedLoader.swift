692ae44e37480436c345039927c1c41bc731b6ee
Armand van der Zwan <armand@gappless.com>
2020-10-28 17:18:52 +0100

    RemoteFeedLoader returns connectivity error when client returns error

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,8 +19,13 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        client.get(from: url) { (_) in
-            
+        client.get(from: url) { (error) in
+            switch error {
+            case .success((_, _)):
+                break
+            case .failure(_):
+                completion(.failure(RemoteFeedLoader.Error.connectivity))
+            }
         }
     }
 }
