e38781f3ba9ac5b21de75c49b4f2b3577f74441b
Libor Polehna <polehna.libor@gmail.com>
2020-10-28 15:37:37 +0400

    Added returns invalid data when HTTPClient returns non 200 status code.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,10 +21,12 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { result in
             switch result {
+            case let .success(response):
+                if response.1.statusCode != 200 {
+                    completion(.failure(Error.invalidData))
+                }
             case .failure:
                 completion(.failure(Error.connectivity))
-            default:
-                completion(.success([]))
             }
         }
     }
