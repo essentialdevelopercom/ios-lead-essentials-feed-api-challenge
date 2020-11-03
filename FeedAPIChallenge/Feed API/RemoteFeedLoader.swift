bb97f74ac96b9e0a9db8f18a58c71e27f050f1f6
Miguel <gannasong@gmail.com>
2020-11-03 22:16:32 +0800

    Decode Image data upon 200 response.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -23,14 +23,37 @@ public final class RemoteFeedLoader: FeedLoader {
     client.get(from: url) { (result) in
       switch result {
       case let .success((data, response)):
-        guard response.statusCode == 200, let _ = try? JSONSerialization.jsonObject(with: data) else {
+        guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
           completion(.failure(Error.invalidData))
           return
         }
-        completion(.success([]))
+        completion(.success(root.feed))
       case .failure:
         completion(.failure(Error.connectivity))
       }
     }
+  }
+}
+
+private struct Root: Decodable {
+  private var items: [Image]
+  var feed: [FeedImage] { items.map { $0.feedImage } }
+}
+
+private struct Image: Decodable {
+  private let id: UUID
+  private let description: String?
+  private let location: String?
+  private let url: URL
+
+  var feedImage: FeedImage {
+    return FeedImage(id: id, description: description, location: location, url: url)
+  }
+
+  private enum CodingKeys: String, CodingKey {
+    case id = "image_id"
+    case description = "image_desc"
+    case location = "image_loc"
+    case url = "image_url"
   }
 }
