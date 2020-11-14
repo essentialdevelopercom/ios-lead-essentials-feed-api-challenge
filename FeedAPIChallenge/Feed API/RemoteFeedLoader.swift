000c0849cd12c3643696639f9effe14368a58375
Tsungyu Yu <ytyubox@gmail.com>
2020-11-14 15:01:22 +0800

    Rafactor: clean self if not need

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,7 +19,7 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        client.get(from: self.url) {
+        client.get(from: url) {
             [completion] (result: Result) in
             switch result {
             case let .success((_, response)):
