e51541d9413a540d0a7db2410a0b9e30a197aaf1
Marcelo Carvalho <marcelodvlp@gmail.com>
2020-11-24 21:59:46 -0300

    Refactor: Extract code to helper method to simplify production code

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -5,51 +5,53 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
-	private let url: URL
-	private let client: HTTPClient
-    
+    private let url: URL
+    private let client: HTTPClient
     private let statusCodeOk: Int = 200
-	
-	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
-	}
-		
-	public init(url: URL, client: HTTPClient) {
-		self.url = url
-		self.client = client
-	}
-	
-	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+        
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url, completion: { [weak self] result in
-            
             switch result {
+            case let .success((data, response)):
+                let feedResult = self!.success(for: data, with: response)
+                completion(feedResult)
             case .failure:
                 completion(.failure(Error.connectivity))
-                
-            case let .success((data, response)):
-                if response.statusCode != self?.statusCodeOk {
-                    completion(.failure(Error.invalidData))
-                
-                } else if (try? JSONSerialization.jsonObject(with: data, options: [])) != nil {
-                    do {
-                        let decoder = JSONDecoder()
-                        let feedItemRoot = try decoder.decode(FeedItemRoot.self, from: data)
-                        
-                        var feedimageList: [FeedImage] = []
-                        
-                        feedItemRoot.items.forEach({ item in
-                            let feedImage = FeedImage(id: item.imageId, description: item.desc, location: item.location, url: URL(string: item.url)!)
-                            feedimageList.append(feedImage)
-                        })
-                        completion(.success(feedimageList))
-                    } catch {
-                        return
-                    }
-                } else {
-                    completion(.failure(Error.invalidData))
-                }
             }
         })
+    }
+    
+    private func success(for data: Data, with response: HTTPURLResponse) -> FeedLoader.Result {
+        if response.statusCode == statusCodeOk, let items = try? map(from: data) {
+            return .success(items)
+        } else {
+            return .failure(Error.invalidData)
+        }
+    }
+    
+    private func map(from data: Data) throws -> [FeedImage] {
+        let decoder = JSONDecoder()
+        let feedItemRoot = try decoder.decode(FeedItemRoot.self, from: data)
+        var feedimageList: [FeedImage] = []
+        
+        feedItemRoot.items.forEach({ item in
+            let feedImage = FeedImage(id: item.imageId,
+                                      description: item.desc,
+                                      location: item.location,
+                                      url: URL(string: item.url)!)
+            
+            feedimageList.append(feedImage)
+        })
+        return feedimageList
     }
 }
