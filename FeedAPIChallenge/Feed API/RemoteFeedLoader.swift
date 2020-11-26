1b40ad64331093127333f3a52e606367e9b0a970
Jean M <jean.mouton.nl+github@gmail.com>
2020-11-26 11:22:43 +0100

    Removed API specific detail from FeedImage and aded a `RemoteFeedImage` struct to deal with deseriailising the API response, thereby decoupling the API specific detail from the Feature module.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -26,15 +26,40 @@ public final class RemoteFeedLoader: FeedLoader {
                 completion(.failure(Error.connectivity))
             case let .success((data, response)):
                 if response.statusCode == 200, let feedItems = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(feedItems.items))
+                    let feedImages: [FeedImage] = feedItems.items.map { Self.map(feedItem: $0) }
+                    completion(.success(feedImages))
                 } else {
                     completion(.failure(Error.invalidData))
                 }
             }
         }
     }
+    
+    private static func map(feedItem: RemoteFeedImage) -> FeedImage {
+        return FeedImage(
+            id: feedItem.id,
+            description: feedItem.description,
+            location: feedItem.location,
+            url: feedItem.url)
+    }
 }
 
 struct Root: Decodable {
-    let items: [FeedImage]
+    let items: [RemoteFeedImage]
+}
+
+struct RemoteFeedImage {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let url: URL
+}
+
+extension RemoteFeedImage: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id = "image_id"
+        case description = "image_desc"
+        case location = "image_loc"
+        case url = "image_url"
+    }
 }
