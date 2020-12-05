50569b53c045937540d9b43a967ff2ba755deec8
Akash Sheelavant <ASheelavant@walmart.com>
2020-12-05 16:39:30 +0530

    Handling connectivity error.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -24,7 +24,7 @@ public final class RemoteFeedLoader: FeedLoader {
 			case .success((_, _)):
 				break
 			case .failure(_):				
-				break
+				completion(.failure(Error.connectivity))
 			}
 		}
 	}
