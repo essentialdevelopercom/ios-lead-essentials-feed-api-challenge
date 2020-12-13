0dcc0121c35103c489b36cdc75e4f47ba9c8febf
Bartosz Dolewski <bartosz.dolewski@gmail.com>
2020-12-13 14:03:46 +0100

    Delivering `invalidData` error when JSON payload is corrupted even if HTTP status code is 200

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,10 +21,13 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-				case let .success((_, response)):
+				case let .success((data, response)):
 					switch response.statusCode {
 						case 200:
-							completion(.failure(Error.connectivity))
+							if (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) != nil {
+								completion(.failure(Error.connectivity))
+							}
+							completion(.failure(Error.invalidData))
 						default:
 							completion(.failure(Error.invalidData))
 					}
