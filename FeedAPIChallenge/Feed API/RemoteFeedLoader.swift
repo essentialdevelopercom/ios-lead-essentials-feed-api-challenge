b634afa003e78f547fb9d9e87247109de1bfb8b5
Tung Vu Duc <tungvuduc2805@gmail.com>
2020-12-20 00:51:48 +0700

    Delivers feed items on 200 HTTP response with JSON items

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -31,11 +31,27 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	func map(_ data: Data, _ response: HTTPURLResponse) -> FeedLoader.Result {
-		if response.statusCode == 200, let _ = try? JSONSerialization.jsonObject(with: data) {
-			return .success([])
+		if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
+			return .success(root.items.map{$0.item})
 		} else {
 			return .failure(Error.invalidData)
 		}
 	}
 	
+}
+
+struct Root: Decodable {
+	var items: [RemoteFeedImage]
+
+}
+
+struct RemoteFeedImage: Decodable {
+	let image_id: UUID
+	let image_desc: String?
+	let image_loc: String?
+	let image_url: URL
+	
+	var item: FeedImage {
+		return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+	}
 }
