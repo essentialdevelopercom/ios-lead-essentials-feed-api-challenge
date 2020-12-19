bc841ea43a04a289e7ecd0b82bb5cbcc14716313
Joseph Wardell <jaywardell@mac.com>
2020-12-19 10:45:44 -0500

    RemoteFeedLoader.load() completes with an empty array on 200 status code with valid json data

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -25,12 +25,78 @@ public final class RemoteFeedLoader: FeedLoader {
 		}
 	}
 	
+	private enum StatusCode: Int {
+		case OK = 200
+	}
+	
 	private func map(_ result: HTTPClient.Result) -> FeedLoader.Result {
 		switch result {
 		case .failure:
 			return .failure(Error.connectivity)
-		case .success:
-			return .failure(Error.invalidData)
+		case let .success((data, response)):
+			switch StatusCode(rawValue: response.statusCode) {
+			case .OK:
+				if (try? JSONDecoder().decode(Root.self, from: data)) != nil {
+					return .success([])
+				}
+				return .failure(Error.invalidData)
+			default:
+				return .failure(Error.invalidData)
+			}
 		}
+	}
+	
+	/*
+	Remote Feed Image Spec:
+
+	Property	Type
+	image_id	UUID
+	image_desc	String (optional)
+	image_loc	String (optional)
+	image_url	URL
+
+	*/
+	public struct RemoteFeedImage: Decodable {
+		public let image_id: UUID
+		public let image_desc: String?
+		public let image_loc: String?
+		public let image_url: URL
+	}
+	
+	/*
+	Payload contract:
+
+
+	200 RESPONSE
+
+	{
+		"items": [
+			{
+				"image_id": "a UUID",
+				"image_desc": "a description",
+				"image_loc": "a location",
+				"image_url": "https://a-image.url",
+			},
+			{
+				"image_id": "another UUID",
+				"image_desc": "another description",
+				"image_url": "https://another-image.url"
+			},
+			{
+				"image_id": "even another UUID",
+				"image_loc": "even another location",
+				"image_url": "https://even-another-image.url"
+			},
+			{
+				"image_id": "yet another UUID",
+				"image_url": "https://yet-another-image.url"
+			}
+			...
+		]
+	}
+
+	*/
+	private struct Root: Decodable {
+		let items: [RemoteFeedImage]
 	}
 }
