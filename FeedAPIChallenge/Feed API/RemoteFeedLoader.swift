e6705655c4e6d339df09692e8678495d7be87322
Tiago Amorim <tamorim@smithmicro.com>
2020-12-21 14:22:05 +0000

    Return invalid data error on non 200 HTTPResponse

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -5,22 +5,27 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
-	private let url: URL
-	private let client: HTTPClient
-	
-	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
-	}
-		
-	public init(url: URL, client: HTTPClient) {
-		self.url = url
-		self.client = client
-	}
-	
-	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        client.get(from: url, completion: { _ in
-            completion(.failure(Error.connectivity))
+    private let url: URL
+    private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        client.get(from: url, completion: { resultType in
+            switch resultType {
+            case .success:
+                completion(.failure(Error.invalidData))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
         })
     }
 }
