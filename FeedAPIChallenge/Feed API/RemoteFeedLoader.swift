91506f153bde7656f58ea9c51a776ac4883823ee
Gustavo Gervasio <gustavogervasio@gmail.com>
2020-12-23 18:10:16 -0300

    Create `FeedImagesMapper` that can handle the received data and response.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -25,41 +25,8 @@ public final class RemoteFeedLoader: FeedLoader {
 			case .failure(_):
 				completion(.failure(Error.connectivity))
 			case let .success((data, response)):
-				guard response.statusCode == 200 else {
-					return completion(.failure(Error.invalidData))
-				}
-				guard let root = try? JSONDecoder().decode(Root.self, from: data) else {
-					return completion(.failure(Error.invalidData))
-				}
-				completion(.success(root.images))
+				completion(FeedImagesMapper.map(data, response: response))
 			}
 		}
-	}
-}
-
-private struct Root: Decodable {
-	private let items: [Item]
-
-	var images: [FeedImage] {
-		return items.map({$0.image})
-	}
-}
-
-private struct Item: Decodable {
-
-	private let id: UUID
-	private let description: String?
-	private let location: String?
-	private let url: URL
-
-	enum CodingKeys: String, CodingKey {
-		case id = "image_id"
-		case description = "image_desc"
-		case location = "image_loc"
-		case url = "image_url"
-	}
-
-	var image: FeedImage {
-		return FeedImage(id: id, description: description, location: location, url: url)
 	}
 }
