5ef151a6901b725faac1c7843b1986b4533375dd
Jorge Lucena <jorgifumi@gmail.com>
2020-12-30 20:02:37 +0100

    Move Mapping logic to a new type `FeedImagesMapper`

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,9 +22,8 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { result in
 			switch result {
 			case let .success((data, response)):
-				if response.statusCode == 200,
-				   let root = try? JSONDecoder().decode(Root.self, from: data) {
-					completion(.success(root.items.map { $0.image }))
+				if let images = try? FeedImagesMapper.map(data, response) {
+					completion(.success(images))
 				} else {
 					completion(.failure(Error.invalidData))
 				}
@@ -35,26 +34,35 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 }
 
-private struct Root: Decodable {
-	var items: [Image]
-}
-
-private struct Image {
-	let id: UUID
-	let description: String?
-	let location: String?
-	let url: URL
-
-	var image: FeedImage {
-		return FeedImage(id: id, description: description, location: location, url: url)
+private class FeedImagesMapper {
+	private struct Root: Decodable {
+		let items: [Image]
 	}
-}
 
-extension Image: Decodable {
-	private enum CodingKeys: String, CodingKey {
-		case id = "image_id"
-		case description = "image_desc"
-		case location = "image_loc"
-		case url = "image_url"
+	private struct Image: Decodable {
+		let id: UUID
+		let description: String?
+		let location: String?
+		let url: URL
+
+		var image: FeedImage {
+			return FeedImage(id: id, description: description, location: location, url: url)
+		}
+
+		private enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case url = "image_url"
+		}
+	}
+
+	static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
+
+		guard response.statusCode == 200 else {
+			throw RemoteFeedLoader.Error.invalidData
+		}
+
+		return try JSONDecoder().decode(Root.self, from: data).items.map { $0.image }
 	}
 }
