9de26b71d82c37ede3dd61df5e0f80daf43f1570
TylerSchwartzman <tschwartzman.dev@gmail.com>
2020-12-30 11:43:54 -0600

    Load method delivers `invalidData` error on 200 HTTPResponse with invalid JSON

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,8 +21,10 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case let .success((_, response)):
+			case let .success((data, response)):
 				if response.statusCode != 200 {
+					completion(.failure(Error.invalidData))
+				} else if !JSONSerialization.isValidJSONObject(data) {
 					completion(.failure(Error.invalidData))
 				}
 			case .failure:
