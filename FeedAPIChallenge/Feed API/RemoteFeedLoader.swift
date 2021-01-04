e035434733203d0b08cbca11ed20c7dbdc4b0a14
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-01-04 09:50:34 +0700

    remove tuple warning

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,7 +21,7 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success(let data, let response):
+			case .success((let data, let response)):
 				if response.statusCode == 200 {
 					do {
 						let root = try JSONDecoder().decode(Root.self, from: data)
