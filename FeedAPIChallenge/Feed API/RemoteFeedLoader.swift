1cde66efca2fb03dabcf1b15a39df22375326e80
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-01-04 09:37:58 +0700

    successfully delivers empty item json on 200 HTTP response code

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,10 +19,19 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
 			switch result {
-			case .success:
-				completion(.failure(Error.invalidData))
+			case .success(let data, let response):
+				if response.statusCode == 200 {
+					if let _ = try? JSONSerialization.jsonObject(with: data) {
+						completion(.success([]))
+					} else {
+						completion(.failure(Error.invalidData))
+					}
+				} else {
+					completion(.failure(Error.invalidData))
+				}
+				
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
