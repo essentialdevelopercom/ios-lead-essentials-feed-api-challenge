812b0587b66271eeaf5e43ac347141336a2cf764
Ivan <ivan.ornes@gmail.com>
2021-01-10 23:16:06 +0100

    Created a WeakHTTPClient typealias to allow weak references to the HTTPClient from the RemoteFeedLoader class When the callback is fired, it checks if the client is still non nil otherwise returns doing nothing

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -4,22 +4,25 @@
 
 import Foundation
 
+public typealias WeakHTTPClient = HTTPClient & AnyObject
+
 public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
-	private let client: HTTPClient
+	private weak var client: WeakHTTPClient?
 	
 	public enum Error: Swift.Error {
 		case connectivity
 		case invalidData
 	}
 	
-	public init(url: URL, client: HTTPClient) {
+	public init(url: URL, client: WeakHTTPClient?) {
 		self.url = url
 		self.client = client
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client?.get(from: url) { [weak self] result in
+			guard self?.client != nil else { return }
 			do {
 				switch result {
 				case .success((let data, let response)):
