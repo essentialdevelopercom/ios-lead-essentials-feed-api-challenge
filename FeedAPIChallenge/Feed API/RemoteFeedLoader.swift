bd1da04115a491cee1a8f9415988486aa419cf8a
ilhan sarı <ilhansari@teknasyon.com>
2021-01-10 18:41:48 +0300

    Delivers invalid data error non 200 HTTPResponse with FeedItemMapper

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,11 +22,40 @@ public final class RemoteFeedLoader: FeedLoader {
     client.get(from: url) { [weak self] result in
       guard self != nil else { return }
       switch result {
-      case .success:
-        break
+      case let .success((data, response)):
+        completion(FeedItemMapper.map(data, from: response))
       case .failure:
         completion(.failure(RemoteFeedLoader.Error.connectivity))
       }
     }
+  }
+}
+
+internal final class FeedItemMapper {
+  struct Root: Decodable {
+    let items: [Item]
+
+    var feed: [FeedImage] {
+      return items.map({ $0.item })
+    }
+  }
+
+  struct Item: Decodable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let url: URL
+
+    var item: FeedImage {
+      return FeedImage(id: id, description: description, location: location, url: url)
+    }
+  }
+
+  private static var OK_200: Int { return 200}
+
+  internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+    guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else { return .failure(RemoteFeedLoader.Error.invalidData)}
+
+    return .success(root.feed)
   }
 }
