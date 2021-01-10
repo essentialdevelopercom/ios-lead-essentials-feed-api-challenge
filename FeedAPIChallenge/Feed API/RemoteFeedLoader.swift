b6b516f26ff7a75e7b0018acde5a32afc8c67e00
Ivan <ivan.ornes@gmail.com>
2021-01-10 21:32:58 +0100

    Passing the deliversInvalidDataErrorOnNon200HTTPResponse test by returning a failure with invalidData error on the success result

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,6 +19,11 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { _ in completion(.failure(Error.connectivity)) }
+		client.get(from: url) { result in
+			switch result {
+			case .success(_): completion(.failure(Error.invalidData))
+			case .failure(_): completion(.failure(Error.connectivity))
+			}
+		}
 	}
 }
