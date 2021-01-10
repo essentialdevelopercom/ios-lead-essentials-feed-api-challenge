1c64fdeba29f4a9431b31a33ec6374975c1e105b
ilhan sarı <ilhansari@teknasyon.com>
2021-01-10 20:34:41 +0300

    Fix space code block

--- FeedAPIChallenge/Feed API/FeedItemMapper.swift
+++ FeedAPIChallenge/Feed API/FeedItemMapper.swift
@@ -27,6 +27,7 @@ internal final class FeedItemMapper {
       return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
     }
   }
+
   private static var OK_200: Int { return 200}
 
   internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
