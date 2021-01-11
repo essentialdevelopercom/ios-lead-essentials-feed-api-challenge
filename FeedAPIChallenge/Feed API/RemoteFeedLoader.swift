f4308cd9213e69be9a481bcd956debb830e69906
Shilpa Bansal <shilpa.bansal@walmartlabs.com>
2021-01-11 21:42:26 +0530

    Uncommented test cases and updated code to pass test cases

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -18,7 +18,51 @@ public final class RemoteFeedLoader: FeedLoader {
 		self.client = client
 	}
 	
+	static let OK_200 = 200
+	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		
+		client.get(from: url) {[weak self] result in
+			guard let strongSelf = self else { return }
+			switch result {
+			case .success(let feedData):
+				if feedData.1.statusCode == RemoteFeedLoader.OK_200 {
+					completion(strongSelf.mapFeedImage(feedData.0))
+				}
+				else {
+					completion(.failure(Error.invalidData))
+				}
+				
+			case .failure(_):
+				completion(.failure(Error.connectivity))
+			}
+		}
+	}
+}
+
+extension RemoteFeedLoader {
+	func mapFeedImage(_ feedData: Data) -> FeedLoader.Result {
+		do {
+			if let json = try JSONSerialization.jsonObject(with: feedData, options: []) as? [String: Any],
+			   let items = json["items"] as? [[String: Any]] {
+				
+				var feedItems: [FeedImage] = []
+				items.forEach({
+					if let image_id = $0["image_id"] as? String,
+					   let image_url = $0["image_url"] as? String {
+						let image_desc = $0["image_desc"] as? String
+						let image_loc = $0["image_loc"] as? String
+							
+						
+						feedItems.append(FeedImage(id: UUID(uuidString: image_id)!,
+										 description: image_desc,
+										 location: image_loc,
+										 url: URL(string: image_url)!))
+					}
+				})
+				return .success(feedItems)
+			}
+		} catch {
+		}
+		return .failure(Error.invalidData)
 	}
 }
