a69e75de24d6ee3af22b7bad7a75db9013187b4e
Tak Mazarura <takmazarura@hotmail.co.uk>
2021-01-12 18:28:00 +0000

    Refracted feed image to be decodable via an extension only (removing unnecessary code as the types native)

--- FeedAPIChallenge/Feed Feature/FeedImage.swift
+++ FeedAPIChallenge/Feed Feature/FeedImage.swift
@@ -4,18 +4,11 @@
 
 import Foundation
 
-public struct FeedImage: Decodable, Hashable {
+public struct FeedImage: Hashable {
 	public let id: UUID
 	public let description: String?
 	public let location: String?
 	public let url: URL
-	
-	enum CodingKeys: String, CodingKey {
-		case id = "image_id"
-		case description = "image_desc"
-		case location = "image_loc"
-		case url = "image_url"
-	}
 	
 	public init(id: UUID, description: String?, location: String?, url: URL) {
 		self.id = id
@@ -23,13 +16,13 @@ public struct FeedImage: Decodable, Hashable {
 		self.location = location
 		self.url = url
 	}
-	
-	public init(from decoder:Decoder) throws {
-		let values = try decoder.container(keyedBy: CodingKeys.self)
-		id = try values.decode(UUID.self, forKey: .id)
-		description = try values.decode(String.self, forKey: .description)
-		location = try values.decode(String.self, forKey: .description)
-		url = try values.decode(URL.self, forKey: .url)
-	}
 }
 
+extension FeedImage: Decodable {
+	private enum CodingKeys: String, CodingKey {
+		case id = "image_id"
+		case description = "image_desc"
+		case location = "image_loc"
+		case url = "image_url"
+	}
+}
