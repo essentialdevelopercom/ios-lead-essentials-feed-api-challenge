46e0f5aa5463b9b3e49f27e3777d2ecdcba088c2
Ivan <ivan.ornes@gmail.com>
2021-01-12 20:48:55 +0100

    Removed optional mark from the HTTPClient reference type in the RemoteFeedLoader

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -6,20 +6,20 @@ import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
-	private var client: HTTPClient?
+	private var client: HTTPClient
 	
 	public enum Error: Swift.Error {
 		case connectivity
 		case invalidData
 	}
 	
-	public init(url: URL, client: HTTPClient?) {
+	public init(url: URL, client: HTTPClient) {
 		self.url = url
 		self.client = client
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client?.get(from: url) { [weak self] result in
+		client.get(from: url) { [weak self] result in
 			guard self != nil else { return }
 			switch result {
 			case .success((let data, let response)):
