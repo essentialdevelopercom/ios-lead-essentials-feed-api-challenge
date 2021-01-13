ae70499a031d744a2c361f4cef02d286d16160aa
aloksbd <30898579+aloksbd@users.noreply.github.com>
2021-01-13 15:41:22 +0545

    Move map logic to 'FeedImageMapper'

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -8,6 +8,30 @@ public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
 	
+	public enum Error: Swift.Error {
+		case connectivity
+		case invalidData
+	}
+	
+	public init(url: URL, client: HTTPClient) {
+		self.url = url
+		self.client = client
+	}
+	
+	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+		client.get(from: self.url){[weak self] result in
+			guard let self = self else {return}
+			switch result{
+			case .failure:
+				completion(.failure(Error.connectivity))
+			case let .success((data, response)):
+				completion(FeedImageMapper.map(data, response))
+			}
+		}
+	}
+}
+
+private class FeedImageMapper{
 	private struct root: Decodable{
 		let items: [Image]
 		
@@ -27,33 +51,11 @@ public final class RemoteFeedLoader: FeedLoader {
 		}
 	}
 	
-	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
-	}
-	
-	public init(url: URL, client: HTTPClient) {
-		self.url = url
-		self.client = client
-	}
-	
-	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: self.url){[weak self] result in
-			guard let self = self else {return}
-			switch result{
-			case .failure:
-				completion(.failure(Error.connectivity))
-			case let .success((data, response)):
-				completion(self.map(data, response))
-			}
-		}
-	}
-	
-	private func map( _ data: Data, _ response: HTTPURLResponse) -> FeedLoader.Result {
+	static fileprivate func map( _ data: Data, _ response: HTTPURLResponse) -> FeedLoader.Result {
 		if response.statusCode == 200, let root = try? JSONDecoder().decode(root.self, from: data){
 			return .success(root.feedItems)
 		}else{
-			return .failure(Error.invalidData)
+			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
 	}
 }
