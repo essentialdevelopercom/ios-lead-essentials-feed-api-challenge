0b168efab76713f674588f3eb005dc64ea0659e5
martyn.pekala <martyn.pekala@farmaprom.pl>
2021-01-13 21:37:35 +0100

    Deliver proper error on non 200 HTTPResponse

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -8,6 +8,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
 	
+	static let OK_200 = 200
+	
 	public enum Error: Swift.Error {
 		case connectivity
 		case invalidData
@@ -19,8 +21,15 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { _ in
-			completion(.failure(Error.connectivity))
+		client.get(from: url) { result in
+			switch result {
+			case let .success((_, response)):
+				if response.statusCode != RemoteFeedLoader.OK_200 {
+					completion(.failure(Error.invalidData))
+				}
+			case .failure:
+				completion(.failure(Error.connectivity))
+			}
 		}
 	}
 }
