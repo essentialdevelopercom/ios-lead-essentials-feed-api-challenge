2e37422f1fd1c86530d961800b80bb8adadca157
Ivan <ivan.ornes@gmail.com>
2021-01-13 21:54:41 +0100

    Reverted var to let

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -6,7 +6,7 @@ import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
-	private var client: HTTPClient
+	private let client: HTTPClient
 	
 	public enum Error: Swift.Error {
 		case connectivity
