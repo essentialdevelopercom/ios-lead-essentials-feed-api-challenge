ce91ed0b674f9942a4e6643fdd4084345c48b278
Ana Nogal <ana.nogal@gmail.com>
2021-01-14 13:15:49 +0100

    put back 4 spaces indentation

--- FeedAPIChallenge/Feed API/ItemsToFeedImagesMapper.swift
+++ FeedAPIChallenge/Feed API/ItemsToFeedImagesMapper.swift
@@ -9,31 +9,31 @@
 import Foundation
 
 struct ItemsToFeedImagesMapper {
-  private static let STATUS_CODE_OK = 200
-
-  private struct Items: Decodable {
-    let items: [Item]
-
-    var feedImages: [FeedImage] {
-      items.map { item in item.feedImage }
+    private static let STATUS_CODE_OK = 200
+    
+    private struct Items: Decodable {
+        let items: [Item]
+        
+        var feedImages: [FeedImage] {
+            items.map { item in item.feedImage }
+        }
     }
-  }
-
-  private struct Item: Decodable {
-    let image_id: UUID
-    let image_desc: String?
-    let image_loc: String?
-    let image_url: URL
-
-    var feedImage: FeedImage {
-      FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+    
+    private struct Item: Decodable {
+        let image_id: UUID
+        let image_desc: String?
+        let image_loc: String?
+        let image_url: URL
+        
+        var feedImage: FeedImage {
+            FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+        }
     }
-  }
-
-  static func map(_ data: Data, from response: HTTPURLResponse) -> FeedLoader.Result {
-    guard response.statusCode == STATUS_CODE_OK, let items = try? JSONDecoder().decode(Items.self, from: data) else {
-      return .failure(RemoteFeedLoader.Error.invalidData)
+    
+    static func map(_ data: Data, from response: HTTPURLResponse) -> FeedLoader.Result {
+        guard response.statusCode == STATUS_CODE_OK, let items = try? JSONDecoder().decode(Items.self, from: data) else {
+            return .failure(RemoteFeedLoader.Error.invalidData)
+        }
+        return .success(items.feedImages)
     }
-    return .success(items.feedImages)
-  }
 }
