cf06b13fe716d22da4237a4928437ab18e5d0314
Kasper Josefsen <Kasper@greenerpastures.dk>
2021-01-17 15:23:39 +0100

    Delivers empty array in success response completion on HTTP 200 status.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -7,7 +7,8 @@ import Foundation
 public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
-	
+	private static let HTTP200Status = 200
+
 	public enum Error: Swift.Error {
 		case connectivity
 		case invalidData
@@ -21,8 +22,12 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success((_, _)):
-				completion(.failure(Error.invalidData))
+			case let .success((_, response)):
+				if response.statusCode == RemoteFeedLoader.HTTP200Status {
+					completion(.success([]))
+				} else {
+					completion(.failure(Error.invalidData))
+				}
 			case .failure(_):
 				completion(.failure(Error.connectivity))
 			}
