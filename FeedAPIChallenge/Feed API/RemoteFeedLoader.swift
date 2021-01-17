195bc578005d37c38731868d8476fc6342404972
Kasper Josefsen <Kasper@greenerpastures.dk>
2021-01-17 16:11:28 +0100

    Implemented `loadFeedItems` to decode and parse response data, via `FeedItemsRoot` and `Items` struct models.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -7,7 +7,7 @@ import Foundation
 public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
-	private static let HTTP200Status = 200
+	private let OK200 = 200
 
 	public enum Error: Swift.Error {
 		case connectivity
@@ -20,17 +20,44 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
 			switch result {
-			case let .success((_, response)):
-				if response.statusCode == RemoteFeedLoader.HTTP200Status {
-					completion(.success([]))
+			case let .success((data, response)):
+				if response.statusCode == self?.OK200,
+					let feedItems = try? self?.loadFeedItems(from: data) {
+					completion(.success(feedItems))
 				} else {
 					completion(.failure(Error.invalidData))
 				}
-			case .failure(_):
+			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
     }
+
+	private func loadFeedItems(from data: Data) throws -> [FeedImage] {
+		try JSONDecoder().decode(FeedItemsRoot.self, from: data)
+			.items
+			.map { $0.feedImage }
+	}
+}
+
+private struct FeedItemsRoot: Decodable {
+	let items: [Items]
+}
+
+private struct Items: Decodable {
+	let image_id: UUID
+	let image_desc: String?
+	let image_loc: String?
+	let image_url: URL
+
+	var feedImage: FeedImage {
+		FeedImage(
+			id: image_id,
+			description: image_desc,
+			location: image_loc,
+			url: image_url
+		)
+	}
 }
