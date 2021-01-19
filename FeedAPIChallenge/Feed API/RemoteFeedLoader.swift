366e00fe59e9ddaca2ce4c83385adc5bc2241715
codebendr-product-designer <codebendr@gmail.comm>
2021-01-19 08:06:06 +0000

    Delivers connectivity error on when the client encounters an error

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -5,27 +5,27 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
-	private let url: URL
-	private let client: HTTPClient
-	
-	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
-	}
-	
-	public init(url: URL, client: HTTPClient) {
-		self.url = url
-		self.client = client
-	}
-	
-	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
-			switch result {
-			case .success((_, _)) :
-				completion(.success([]))
-			case let .failure(error) :
-				completion(.failure(error))
-			}
-		}
-	}
+    private let url: URL
+    private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        client.get(from: url) { result in
+            switch result {
+            case .success((_, _)) :
+                completion(.success([]))
+            case .failure(_) :
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
 }
