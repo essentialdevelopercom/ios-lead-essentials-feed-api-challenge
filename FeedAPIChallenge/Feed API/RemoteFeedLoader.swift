3d37912ac5fad1aa7c0f25b459c58d01dfdb34b2
martyn.pekala <martyn.pekala@farmaprom.pl>
2021-01-23 10:44:39 +0100

    Return empty list when JSON is valid but contains no elements

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -23,11 +23,20 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case let .success((_, response)):
-				completion(.failure(Error.invalidData))
+			case let .success((data, response)):
+				guard response.statusCode == RemoteFeedLoader.OK_200,
+					let root = try? JSONDecoder().decode(Root.self, from: data) else {
+					completion(.failure(Error.invalidData))
+					return
+				}
+				completion(.success(root.items))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
 	}
+}
+
+struct Root: Decodable {
+	let items: [FeedImage]
 }
