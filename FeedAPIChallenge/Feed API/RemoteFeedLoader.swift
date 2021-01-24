cb8475dcf05e504f43a1c8c46170f8a4f1607d3b
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-01-24 11:55:24 +0100

    Delivers success with items on 200 HTTP response with a valid JSON

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,18 +19,14 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url, completion: { result in
+		client.get(from: url) { [weak self] result in
+			guard self != nil else { return }
 			switch result {
 			case let .success((data, response)):
-				guard response.statusCode == 200, let _ = try? JSONSerialization.jsonObject(with: data) else {
-					completion(.failure(Error.invalidData))
-					return
-				}
-				completion(.success([]))
-				
+				completion(FeedImagesMapper.map(data, from: response))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
-		})
+		}
 	}
 }
