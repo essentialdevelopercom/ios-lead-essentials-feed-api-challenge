caa3d38291ebded760f158344ea08a72eff3e19f
Thiago Penna <thiago.penna@icloud.com>
2021-02-03 18:54:24 -0300

    Nest remote model objects into mapper

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -33,37 +33,37 @@ public final class RemoteFeedLoader: FeedLoader {
 private struct RemoteFeedMapper {
 	static func map(_ data: Data, response: HTTPURLResponse) -> FeedLoader.Result {
 		guard response.statusCode == 200,
-			  let feed = try? JSONDecoder().decode(RemoteFeedResponseRootObject.self, from: data) else {
+			  let feed = try? JSONDecoder().decode(RootObject.self, from: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
 		return .success(feed.images)
 	}
-}
-
-private struct RemoteFeedResponseRootObject: Decodable {
-	let items: [RemoteFeedResponseImageObject]
 	
-	var images: [FeedImage] {
-		items.compactMap { item in
-			FeedImage(id: item.id,
-					  description: item.description,
-					  location: item.location,
-					  url: item.url)
-			
+	private struct RootObject: Decodable {
+		let items: [Image]
+		
+		var images: [FeedImage] {
+			items.compactMap { item in
+				FeedImage(id: item.id,
+						  description: item.description,
+						  location: item.location,
+						  url: item.url)
+				
+			}
 		}
 	}
-}
-
-private struct RemoteFeedResponseImageObject: Decodable {
-	let id: UUID
-	let description: String?
-	let location: String?
-	let url: URL
 	
-	enum CodingKeys: String, CodingKey {
-		case id = "image_id"
-		case description = "image_desc"
-		case location = "image_loc"
-		case url = "image_url"
+	private struct Image: Decodable {
+		let id: UUID
+		let description: String?
+		let location: String?
+		let url: URL
+		
+		enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case url = "image_url"
+		}
 	}
 }
