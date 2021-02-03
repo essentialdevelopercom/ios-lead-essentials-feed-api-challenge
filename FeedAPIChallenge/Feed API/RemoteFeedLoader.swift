d3ec5ce4b0da888aac8ad9e76534e4b40cc8f51a
Thiago Penna <thiago.penna@icloud.com>
2021-02-03 18:19:50 -0300

    Deliver `invalidData` error when data is not a valid JSON

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,8 +21,9 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		self.client.get(from: url) { result in
 			switch result {
-			case let .success((_, response)):
-				guard response.statusCode == 200 else {
+			case let .success((data, response)):
+				guard response.statusCode == 200,
+					  ((try? JSONSerialization.jsonObject(with: data)) != nil) else {
 					completion(.failure(Error.invalidData))
 					return
 				}
