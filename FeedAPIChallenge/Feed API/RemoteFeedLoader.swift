687d72e5b25526f7e86218890f3879c7e51bc4a8
Thiago Penna <thiago.penna@icloud.com>
2021-02-03 19:02:09 -0300

    Implement weak self check on `RemoteFeedLoader` `load` method to prevent memory leaks

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,7 +19,10 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		self.client.get(from: url) { result in
+		self.client.get(from: url) { [weak self] result in
+			guard self != nil else {
+				return
+			}
 			switch result {
 			case let .success((data, response)):
 				completion(RemoteFeedMapper.map(data, response: response))
