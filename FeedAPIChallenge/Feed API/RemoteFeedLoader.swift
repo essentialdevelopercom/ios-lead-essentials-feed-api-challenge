1e7bfc12fc18cbdb08e05b91245052ac538d6c20
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-02-04 21:50:36 +0100

    Parse and deliver JSON objects in `RemoteFeedLoader.load`.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,13 +19,29 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		self.client.get(from: self.url) { result in
+		self.client.get(from: self.url) { [weak self] result in
 			switch result {
 			case .failure:
 				completion(.failure(Error.connectivity))
-			case .success:
-				completion(.failure(Error.invalidData))
+			case let .success((data, response)):
+				switch response.statusCode {
+				case 200:
+					if let images = self?.images(from: data) {
+						completion(.success(images))
+					} else {
+						completion(.failure(Error.invalidData))
+					}
+				default:
+					completion(.failure(Error.invalidData))
+				}
 			}
 		}
+	}
+
+	// MARK: Private methods
+
+	private func images(from data: Data) -> [FeedImage]? {
+		let root = try? JSONSerialization.jsonObject(with: data) as? [String: [FeedImage]]
+		return root?["items"]
 	}
 }
