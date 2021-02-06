26a931f3689d90649724e2bdb0fea79264c197e7
Peter Barclay <peter@peter-barclay.com>
2021-02-06 16:25:03 +0000

    RemoteFeedLoader - complete with empty feed for 200 response status
    Test for scenario where response status is 200 and data is empty items array

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,13 +19,38 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
-			switch result {
-			case .failure:
-				completion(.failure(Error.connectivity))
-			case .success:
-				completion(.failure(Error.invalidData))
+		client.get(from: url) { clientResult in
+			do {
+				let feed = try Self.process(clientResult)
+				completion(.success(feed))
+			} catch {
+				completion(.failure(error))
 			}
+		}
+	}
+	
+	private static func process(_ result: HTTPClient.Result) throws -> [FeedImage] {
+		let OK_200 = 200
+		
+		switch result {
+		case .failure:
+			throw Error.connectivity
+		case let .success((data, response)):
+			switch response.statusCode {
+			case OK_200:
+				return try decodeFeed(from: data)
+			default:
+				throw Error.invalidData
+			}
+		}
+	}
+	
+	private static func decodeFeed(from data: Data) throws -> [FeedImage] {
+		do {
+			_ = try JSONSerialization.jsonObject(with: data)
+			return []
+		} catch {
+			throw Error.invalidData
 		}
 	}
 }
