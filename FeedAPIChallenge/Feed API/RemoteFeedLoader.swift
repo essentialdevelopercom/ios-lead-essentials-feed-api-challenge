eed5136eacde19e6d213533a62e06967a1801406
Tomas Jablonskis <tomas.jablonskis6@gmail.com>
2021-02-07 15:08:13 +0200

    Moved `FeedImage` mapping logic into separate `FeedImageLoader` class

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,9 +22,10 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { result in
 			switch result {
 			case let .success((data, response)):
-				if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-					completion(.success(root.feed))
-				} else {
+				do {
+					let feed = try FeedImageMapper.map(data, response)
+					completion(.success(feed))
+				} catch {
 					completion(.failure(Error.invalidData))
 				}
 			case .failure:
@@ -34,33 +35,44 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 }
 
-private struct Root: Decodable {
-	let items: [Item]
+private final class FeedImageMapper {
+	private struct Root: Decodable {
+		let items: [Item]
 
-	var feed: [FeedImage] {
-		items.map { $0.item }
-	}
-}
-
-private struct Item: Decodable {
-	let id: String
-	let description: String?
-	let location: String?
-	let url: URL
-
-	var item: FeedImage {
-		FeedImage(
-			id: UUID(uuidString: id)!,
-			description: description,
-			location: location,
-			url: url
-		)
+		var feed: [FeedImage] {
+			items.map { $0.item }
+		}
 	}
 
-	enum CodingKeys: String, CodingKey {
-		case id = "image_id"
-		case description = "image_desc"
-		case location = "image_loc"
-		case url = "image_url"
+	private struct Item: Decodable {
+		let id: String
+		let description: String?
+		let location: String?
+		let url: URL
+
+		var item: FeedImage {
+			FeedImage(
+				id: UUID(uuidString: id)!,
+				description: description,
+				location: location,
+				url: url
+			)
+		}
+
+		enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case url = "image_url"
+		}
+	}
+
+	static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
+		guard response.statusCode == 200 else {
+			throw RemoteFeedLoader.Error.invalidData
+		}
+
+		let root = try JSONDecoder().decode(Root.self, from: data)
+		return root.feed
 	}
 }
