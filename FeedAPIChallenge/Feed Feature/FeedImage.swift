eb2b5d54ae3014ba993069ff31de6448f2214b96
Zsivics Sanel <sanel.zsivics@kh.hu>
2021-02-07 18:21:54 +0100

    Deliver items array on 200 HTTP response with JSON items

--- FeedAPIChallenge/Feed Feature/FeedImage.swift
+++ FeedAPIChallenge/Feed Feature/FeedImage.swift
@@ -17,3 +17,12 @@ public struct FeedImage: Hashable {
 		self.url = url
 	}
 }
+
+extension FeedImage: Decodable {
+	private enum CodingKeys: String, CodingKey {
+		case id = "image_id"
+		case description = "image_desc"
+		case location = "image_loc"
+		case url = "image_url"
+	}
+}
