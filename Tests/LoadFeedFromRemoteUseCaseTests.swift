70d9e59d5a5a423c05a313cb193e6b3744894169
Mo Al Waili <mohammed.alwaili@gmail.com>
2021-02-09 16:11:28 +0100

    Implement load function to call the client and complete with an empty list of feed images which makes our load twice test succeed

--- Tests/LoadFeedFromRemoteUseCaseTests.swift
+++ Tests/LoadFeedFromRemoteUseCaseTests.swift
@@ -25,15 +25,25 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
 		XCTAssertTrue(client.requestedURLs.isEmpty)
 	}
 	
-	//	func test_loadTwice_requestsDataFromURLTwice() {
-	//		let url = URL(string: "https://a-given-url.com")!
-	//		let (sut, client) = makeSUT(url: url)
-	//
-	//		sut.load { _ in }
-	//		sut.load { _ in }
-	//
-	//		XCTAssertEqual(client.requestedURLs, [url, url])
-	//	}
+		func test_loadTwice_requestsDataFromURLTwice() {
+			// Given
+			let url = URL(string: "https://a-given-url.com")!
+			let (sut, client) = makeSUT(url: url)
+			let exp = expectation(description: "wait for completion")
+			exp.expectedFulfillmentCount = 2
+			let error = NSError(domain: "", code: 0)
+			
+			// When
+			sut.load { _ in exp.fulfill() }
+			client.complete(with: error, at: 0)
+			
+			sut.load { _ in exp.fulfill() }
+			client.complete(with: error, at: 1)
+	
+			// Then
+			wait(for: [exp], timeout: 1.0)
+			XCTAssertEqual(client.requestedURLs, [url, url])
+		}
 	//
 	//	func test_load_deliversConnectivityErrorOnClientError() {
 	//		let (sut, client) = makeSUT()
