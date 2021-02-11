bfa6f505cd1ef5e4d45c35efef16417de45f6b43
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2021-02-11 21:12:59 +0200

    Remove API details from leaking into other modules

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -27,7 +27,7 @@ public final class RemoteFeedLoader: FeedLoader {
 			switch result {
 			case let .success((data, response)):
 				if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-					completion(.success(root.items))
+					completion(.success(root.items.map { $0.item} ))
 				} else {
 					completion(.failure(Error.invalidData))
 				}
@@ -39,5 +39,16 @@ public final class RemoteFeedLoader: FeedLoader {
 }
 
 private struct Root: Decodable {
-	var items: [FeedImage]
+	var items: [Item]
  }
+
+private struct Item: Decodable {
+	let image_id: UUID
+	let image_desc: String?
+	let image_loc: String?
+	let image_url: URL
+
+	var item: FeedImage {
+		FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+	}
+}
