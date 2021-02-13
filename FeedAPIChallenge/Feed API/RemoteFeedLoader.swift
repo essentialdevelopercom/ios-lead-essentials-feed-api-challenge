05f743e12c8fde294c40804ee806cdd4679e82de
Naveen <naveentg.ntg@gmail.com>
2021-02-13 22:55:32 +0530

    Verifying that the Loader requests data

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,5 +20,9 @@ public final class RemoteFeedLoader: FeedLoader {
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		
+		client.get(from: url) { _ in
+			
+		}
+		
 	}
 }
