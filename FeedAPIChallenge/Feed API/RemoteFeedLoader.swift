e0a4169d9f5d3a4ac844e04a98d195b907b36987
grav0211 <wbiosd111@gmail.com>
2021-02-13 10:43:07 -0500

    Refactor duplicate code so that we complete with an invalidData error on success and complete with a connectivity error on failure.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,13 +21,9 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case let .success((_, response)):
-				if response.statusCode == 200 {
-					return completion(.failure(Error.invalidData))
-				} else {
-					return completion(.failure(Error.invalidData))
-				}
-			case .failure(_):
+			case .success:
+				return completion(.failure(Error.invalidData))
+			case .failure:
 				return completion(.failure(Error.connectivity))
 			}
 		}
