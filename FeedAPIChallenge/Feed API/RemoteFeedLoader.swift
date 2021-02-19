fa9f50ed42f9d2b9e05ffe81236045db971f66f4
Guilherme Bueno <bueno.guis@gmail.com>
2021-02-19 08:35:35 -0300

    Load delivers invalid data error on non 200 HTTPResponse

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -23,8 +23,10 @@ public final class RemoteFeedLoader: FeedLoader {
 			switch result {
 			case .failure(_):
 				completion(.failure(Error.connectivity))
-			default:
-				return
+			case let .success((_, response)):
+				if response.statusCode != 200 {
+					completion(.failure(Error.invalidData))
+				}
 			}
 		}
 	}
