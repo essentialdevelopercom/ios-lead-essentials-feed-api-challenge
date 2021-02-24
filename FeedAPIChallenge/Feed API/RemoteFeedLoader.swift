7fcee26daf3055b8d6d8900326aed93694543b55
Riccardo Rossi <riki.redz@gmail.com>
2021-02-24 23:28:50 +0100

    Adds context to 200 status code and to tuple retrieved from success result.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -18,13 +18,16 @@ public final class RemoteFeedLoader: FeedLoader {
 		self.client = client
 	}
 	
+	private static let OK_200 = 200
+	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
 			case .failure:
 				completion(.failure(Error.connectivity))
 			case .success(let content):
-				if content.1.statusCode != 200 {
+				let response = content.1
+				if response.statusCode != RemoteFeedLoader.OK_200 {
 					completion(.failure(Error.invalidData))
 				}
 			}
