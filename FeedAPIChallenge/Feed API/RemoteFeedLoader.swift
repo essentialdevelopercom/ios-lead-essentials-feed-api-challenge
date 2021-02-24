8134388a618440429b3790306c3fb141265876de
Tulio Parreiras <tulio_parreiras@hotmail.com>
2021-02-24 17:10:35 -0300

    RemoteFeedLoader does deliver result results after instance has been deallocated

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -34,7 +34,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url, completion: { result in
+		client.get(from: url, completion: { [weak self] result in
+			guard self != nil else { return }
 			switch result {
 			case let .success((data, response)):
 				if response.statusCode == 200, let items = try? JSONDecoder().decode(Items.self, from: data) {
