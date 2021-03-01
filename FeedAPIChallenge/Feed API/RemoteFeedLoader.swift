0d505c6a3badb849884792a457ac3a35e201737c
Fabio Mignogna <mig.fabio@gmail.com>
2021-03-01 22:57:52 +0000

    Delivers no items on 200 HTTP Response with empty JSON list

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,11 +21,19 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success:
-				completion(.failure(Error.invalidData))
+			case let .success((data, response)):
+				if response.statusCode == 200, let _ = try? JSONDecoder().decode(Root.self, from: data) {
+					completion(.success([]))
+				} else {
+					completion(.failure(Error.invalidData))
+				}
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
+	}
+
+	private struct Root: Decodable {
+		let items: [FeedImage]
 	}
 }
