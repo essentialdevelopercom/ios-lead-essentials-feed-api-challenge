f0e1196ecb5b4da5f452e54c6958df7d8480774c
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-03-01 10:39:20 +0100

    Delivers InvalidDataError On200HTTPResponse With InvalidJSON

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,14 +21,42 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case let .success((_, response)):
+			case let .success((data, response)):
 				guard response.statusCode == 200 else {
 					completion(.failure(Error.invalidData))
 					return
 				}
+				completion(RemoteFeedMapper.map(data))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
+	}
+}
+
+private struct RemoteFeedMapper {
+	private struct Root: Decodable {
+		let items: [Image]
+	}
+	
+	private struct Image: Decodable {
+		private let id: UUID
+		private let description: String?
+		private let location: String?
+		private let url: String
+		
+		enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case url = "image_url"
+		}
+	}
+	
+	static func map(_ data: Data) -> FeedLoader.Result {
+		guard let _ = try? JSONDecoder().decode(Root.self, from: data) else {
+			return .failure(RemoteFeedLoader.Error.invalidData)
+		}
+		return .failure(RemoteFeedLoader.Error.invalidData)
 	}
 }
