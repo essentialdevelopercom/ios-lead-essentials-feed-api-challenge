4bde7978107211a3b4ee4bd2eed9fbe06430da27
sabiusa <sabakhuts@gmail.com>
2021-03-01 09:54:14 +0400

    `RemoteFeedLoader` delivers success on 200 HTTP response

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,11 +21,45 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success:
-				completion(.failure(Error.invalidData))
+			case let .success((data, response)):
+				if response.statusCode == 200,
+				   let root = try? JSONDecoder().decode(Root.self, from: data)
+				{
+					completion(.success(root.items.map { $0.feedImage }))
+				} else {
+					completion(.failure(Error.invalidData))
+				}
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
 	}
 }
+
+private struct Root: Decodable {
+	let items: [Item]
+}
+
+private struct Item: Decodable {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let url: URL
+	
+	enum CodingKeys: String, CodingKey {
+		case id = "image_id"
+		case description = "image_desc"
+		case location = "image_loc"
+		case url = "image_url"
+	}
+	
+	var feedImage: FeedImage {
+		return FeedImage(
+			id: UUID(),
+			description: nil,
+			location: nil,
+			url: URL(string: "http://another-url.com")!
+		)
+	}
+}
+
