d4490a1cf196f313733ffb78790d42d29579de7c
Matthias Sehrbrock <matthias.sehrbrock@grandcentrix.net>
2021-03-04 22:05:21 +0100

    guarantee RemoteFeedLoader load method delivers invalidData error on non 200 HTTPResponse

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -23,8 +23,12 @@ public final class RemoteFeedLoader: FeedLoader {
 			switch result {
 			case .failure:
 				completion(.failure(Error.connectivity))
-			case .success:
-				completion(.success([]))
+			case let .success((_, response)):
+				if response.statusCode == 200 {
+					completion(.success([]))
+				} else {
+					completion(.failure(Error.invalidData))
+				}
 			}
 		}
 	}
