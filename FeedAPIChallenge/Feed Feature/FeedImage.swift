677839be59f5ee0a966995b3ebe4fa27a4874f96
ksenia <ksenia@bekitzur.com>
2021-03-05 07:04:13 +0300

    deliver array of decoded items

--- FeedAPIChallenge/Feed Feature/FeedImage.swift
+++ FeedAPIChallenge/Feed Feature/FeedImage.swift
@@ -5,6 +5,13 @@
 import Foundation
 
 public struct FeedImage: Hashable, Decodable {
+	enum Keys: String, CodingKey {
+		case id = "image_id"
+		case location = "image_loc"
+		case url = "image_url"
+		case description = "image_desc"
+	}
+	
 	public let id: UUID
 	public let description: String?
 	public let location: String?
@@ -15,5 +22,13 @@ public struct FeedImage: Hashable, Decodable {
 		self.description = description
 		self.location = location
 		self.url = url
+	}
+	
+	public init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: FeedImage.Keys.self)
+		id = try container.decode(UUID.self, forKey: .id)
+		description = try container.decodeIfPresent(String.self, forKey: .description)
+		location = try container.decodeIfPresent(String.self, forKey: .location)
+		url = try container.decode(URL.self, forKey: .url)
 	}
 }
