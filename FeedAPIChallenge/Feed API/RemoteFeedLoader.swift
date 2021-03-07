71b1cf4f8ff461c95050505f5b1847e6ab53ab71
Abdul <rahimi.diallo96@gmail.com>
2021-03-07 11:19:58 -0500

    Extract the decoding logic into a separate structure

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -23,15 +23,21 @@ public final class RemoteFeedLoader: FeedLoader {
 			guard self != nil else { return }
 			switch result {
 			case .success(( let data, let response)):
-				if response.statusCode == 200,
-					let images = try? JSONDecoder().decode(Images.self, from: data) {
-					completion(.success(images.items))
-				} else {
-					completion(.failure(Error.invalidData))
-				}
+				completion(JSONMapper.map(data: data, response: response))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
+		}
+	}
+}
+
+struct JSONMapper {
+	static func map(data: Data, response: HTTPURLResponse) -> FeedLoader.Result {
+		if response.statusCode == 200,
+			let images = try? JSONDecoder().decode(Images.self, from: data) {
+			return .success(images.items)
+		} else {
+			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
 	}
 }
