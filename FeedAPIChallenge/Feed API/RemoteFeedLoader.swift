8a922db94d450e5aab88052ec8e51e0019c9de7a
Abdul <rahimi.diallo96@gmail.com>
2021-03-09 19:46:36 -0500

    Create a root structure that map received JSON into FeedImage. Properly decode data and complete with a failure if it fails

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,13 +21,43 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success((_, let response)):
+			case .success((let data, let response)):
 				if response.statusCode != 200 {
 					completion(.failure(Error.invalidData))
+				} else {
+					if let _ = try? JSONDecoder().decode(Root.self, from: data) {
+						return
+					} else {
+						completion(.failure(Error.invalidData))
+					}
 				}
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
 	}
+}
+
+struct Root: Decodable {
+	
+	var images: [FeedImage] {
+		items.map { $0.feedImage }
+	}
+	
+	var items: [Image]
+	
+	struct Image: Decodable {
+		var image_id: UUID
+		var image_desc: String?
+		var image_loc: String?
+		var image_url: URL
+		var feedImage: FeedImage {
+			return FeedImage(
+				id: self.image_id,
+				description: self.image_desc,
+				location: self.image_loc,
+				url: self.image_url)
+		}
+	}
+	
 }
