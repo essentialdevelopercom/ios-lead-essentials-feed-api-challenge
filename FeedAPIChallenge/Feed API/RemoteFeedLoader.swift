d64c27b16a409ab2516dc9d6ddfb12d924de607d
Alix <alexey@dream-tek.io>
2021-04-03 15:26:37 +0300

    added FeedMapper to extract mapping logic from load to separate class

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,11 +22,7 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { (result) in
 			switch result {
 			case .success((let data, let response)):
-				if let root = try? JSONDecoder().decode(Root.self, from: data), response.statusCode == 200 {
-					completion(.success(root.items.map({ $0.image })))
-				} else {
-					completion(.failure(Error.invalidData))
-				}
+				completion(FeedMapper.map(data, response: response))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
@@ -34,25 +30,36 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 }
 
-private struct Root: Decodable {
-	let items: [FeedImageItem]
-}
-
-private struct FeedImageItem: Decodable {
+private class FeedMapper {
 	
-	var image: FeedImage {
-		return FeedImage(id: id, description: description, location: location, url: url)
+	private struct Root: Decodable {
+		let items: [FeedImageItem]
+	}
+
+	private struct FeedImageItem: Decodable {
+		
+		var image: FeedImage {
+			return FeedImage(id: id, description: description, location: location, url: url)
+		}
+		
+		let id: UUID
+		let description: String?
+		let location: String?
+		let url: URL
+		
+		private enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case url = "image_url"
+		}
 	}
 	
-	let id: UUID
-	let description: String?
-	let location: String?
-	let url: URL
-	
-	private enum CodingKeys: String, CodingKey {
-		case id = "image_id"
-		case description = "image_desc"
-		case location = "image_loc"
-		case url = "image_url"
+	static func map(_ data: Data, response: HTTPURLResponse) ->  FeedLoader.Result{
+		if let root = try? JSONDecoder().decode(Root.self, from: data), response.statusCode == 200 {
+			return .success(root.items.map({ $0.image }))
+		} else {
+			return .failure(RemoteFeedLoader.Error.invalidData)
+		}
 	}
 }
