7aa231f0a9c251a5b5c6081b8dff530d6fd81985
Alix <alexey@dream-tek.io>
2021-04-03 15:34:19 +0300

    extracted FeedMapper to separate file

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -30,39 +30,3 @@ public final class RemoteFeedLoader: FeedLoader {
 		}
 	}
 }
-
-private class FeedMapper {
-	
-	private struct Root: Decodable {
-		let items: [FeedImageItem]
-	}
-
-	private struct FeedImageItem: Decodable {
-		
-		var image: FeedImage {
-			return FeedImage(id: id, description: description, location: location, url: url)
-		}
-		
-		let id: UUID
-		let description: String?
-		let location: String?
-		let url: URL
-		
-		private enum CodingKeys: String, CodingKey {
-			case id = "image_id"
-			case description = "image_desc"
-			case location = "image_loc"
-			case url = "image_url"
-		}
-	}
-	
-	private static let OK_200 = 200
-	
-	static func map(_ data: Data, response: HTTPURLResponse) ->  FeedLoader.Result{
-		if let root = try? JSONDecoder().decode(Root.self, from: data), response.statusCode == OK_200 {
-			return .success(root.items.map({ $0.image }))
-		} else {
-			return .failure(RemoteFeedLoader.Error.invalidData)
-		}
-	}
-}
