e2709e751d6ce94f134249b542d9fa8628b5427c
Alok <sinha18alok@gmail.com>
2021-04-07 09:24:20 +0200

    removing extra space before data

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -23,7 +23,7 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { [weak self] (result) in
 			guard self != nil else { return }
 			switch result {
-			case let .success(( data, response)):
+			case let .success((data, response)):
 				completion(Root.feedImagesFromData(from: data, statusCode: response.statusCode))
 			case .failure:
 				completion(.failure(Error.connectivity))
