8cd6da791d85a34a6eb7432cec60b5d350e98281
Andreas Link <mail@linkandreas.de>
2021-04-10 20:06:21 +0200

    Move distributed feed image mapping logic into FeedImageMapper and drop FeedImageEntity

---
+++ FeedAPIChallenge/Feed API/FeedImageMapper.swift
@@ -0,0 +1,53 @@
+//
+//  FeedImageEntity.swift
+//  FeedAPIChallenge
+//
+//  Created by Andreas Link on 10.04.21.
+//  Copyright © 2021 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
+
+enum FeedImageMapper {
+	private enum Constants {
+		static let OK_200: UInt = 200
+	}
+
+	private struct Root: Decodable {
+		let items: [Item]
+	}
+
+	private struct Item: Decodable {
+		enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case url = "image_url"
+		}
+
+		let id: UUID
+		let description: String?
+		let location: String?
+		let url: URL
+	}
+
+	static func map(data: Data, response: HTTPURLResponse) -> FeedLoader.Result {
+		guard response.statusCode == Constants.OK_200 else { return .failure(RemoteFeedLoader.Error.invalidData) }
+
+		do {
+			let root: Root = try JSONDecoder().decode(Root.self, from: data)
+			let images: [FeedImage] = root.items.map { item in
+				return FeedImage(
+					id: item.id,
+					description: item.description,
+					location: item.location,
+					url: item.url
+				)
+			}
+
+			return .success(images)
+		} catch {
+			return .failure(RemoteFeedLoader.Error.invalidData)
+		}
+	}
+}
--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -8,10 +8,6 @@ public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
 
-	private enum Constants {
-		static let OK_200: UInt = 200
-	}
-
 	public enum Error: Swift.Error {
 		case connectivity
 		case invalidData
@@ -24,46 +20,15 @@ public final class RemoteFeedLoader: FeedLoader {
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { [weak self] result in
-			guard let self = self else { return }
+			guard self != nil else { return }
 
 			switch result {
-			case let .success((data, response)) where response.statusCode == Constants.OK_200:
-				completion(self.handleSuccessfulResponseWithStatusCode200(data: data))
-
-			case .success:
-				completion(.failure(Error.invalidData))
+			case let .success((data, response)):
+				completion(FeedImageMapper.map(data: data, response: response))
 
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
 	}
-
-	private func handleSuccessfulResponseWithStatusCode200(data: Data) -> FeedLoader.Result {
-		do {
-			let root: Root = try JSONDecoder().decode(Root.self, from: data)
-			return .success(FeedImageMapper.map(from: root.items))
-		} catch {
-			return .failure(Error.invalidData)
-		}
-	}
-}
-
-private enum FeedImageMapper {
-	static func map(from entities: [FeedImageEntity]) -> [FeedImage] {
-		return entities.map(map(from:))
-	}
-
-	static func map(from entity: FeedImageEntity) -> FeedImage {
-		return .init(
-			id: entity.id,
-			description: entity.description,
-			location: entity.location,
-			url: entity.url
-		)
-	}
-}
-
-private struct Root: Decodable {
-	let items: [FeedImageEntity]
 }
