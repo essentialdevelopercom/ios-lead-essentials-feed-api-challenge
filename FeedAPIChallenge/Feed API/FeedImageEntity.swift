7f0f22946f568cab5a1229a2e0aa9cb8853f0698
Andreas Link <mail@linkandreas.de>
2021-04-10 12:18:52 +0200

    Add FeedImageEntity to separate Api from domain model.

---
+++ FeedAPIChallenge/Feed API/FeedImageEntity.swift
@@ -0,0 +1,23 @@
+//
+//  FeedImageEntity.swift
+//  FeedAPIChallenge
+//
+//  Created by Andreas Link on 10.04.21.
+//  Copyright © 2021 Essential Developer Ltd. All rights reserved.
+//
+
+struct FeedImageEntity: Decodable {
+	let id: String
+	let description: String?
+	let location: String?
+	let imageUrl: String
+}
+
+extension FeedImageEntity {
+	enum CodingKeys: String, CodingKey {
+		case id = "image_id"
+		case description = "image_desc"
+		case location = "image_loc"
+		case imageUrl = "image_url"
+	}
+}
