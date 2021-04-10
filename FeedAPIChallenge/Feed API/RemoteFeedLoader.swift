5f4e7c1d0aed05d82c331d5327a1df0dd6bd402a
Andreas Link <mail@linkandreas.de>
2021-04-10 10:58:46 +0200

    Call completion with invalid data in RemoteFeedLoader's load function when client succeeded to satisfy load_deliversInvalidDataErrorOn200HTTPResponseWithInvalidJSON unit test

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,14 +21,11 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case let .success((_, response)) where response.statusCode != 200:
+			case .success:
 				completion(.failure(Error.invalidData))
 
 			case .failure:
 				completion(.failure(Error.connectivity))
-
-			default:
-				break
 			}
 		}
 	}
