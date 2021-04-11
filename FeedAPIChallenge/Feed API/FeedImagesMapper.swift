cbd28c67e156f4459dfea91def16a60d4c1c7b9b
Lívia Vasconcelos <liu.arquiteta@gmail.com>
2021-04-11 15:07:26 -0300

    Fix indentation on map method.

--- FeedAPIChallenge/Feed API/FeedImagesMapper.swift
+++ FeedAPIChallenge/Feed API/FeedImagesMapper.swift
@@ -35,8 +35,7 @@ internal final class FeedImagesMapper {
 		}
 	}
 	
-	internal static func map(_ data: Data,
-							 from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+	internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
 		guard response.statusCode == 200,
 			  let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData)
