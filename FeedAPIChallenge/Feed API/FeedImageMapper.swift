95b6eca2a9f818270299f4dd24a9d5a533e28d97
Vladimir Jeremic <vladimir@ezderm.com>
2021-04-13 17:58:04 +0200

    FeedImageLoader delivers items/empty array on succes

--- FeedAPIChallenge/Feed API/FeedImageMapper.swift
+++ FeedAPIChallenge/Feed API/FeedImageMapper.swift
@@ -11,6 +11,10 @@ import Foundation
 internal final class FeedImageMapper {
 	private struct RootItem: Decodable {
 		let items: [FeedImageItem]
+
+		var feedImages: [FeedImage] {
+			return items.map({ $0.feedImage })
+		}
 	}
 
 	private struct FeedImageItem: Decodable {
@@ -18,16 +22,20 @@ internal final class FeedImageMapper {
 		let image_desc: String?
 		let image_loc: String?
 		let image_url: URL
+
+		var feedImage: FeedImage {
+			return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+		}
 	}
 
 	private static let OK_200_HTTP_SATUS_CODE = 200
 
 	internal static func map(_ data: Data, from response: HTTPURLResponse) -> FeedLoader.Result {
 		guard FeedImageMapper.OK_200_HTTP_SATUS_CODE == response.statusCode,
-		      let _ = try? JSONDecoder().decode(RootItem.self, from: data) else {
+		      let rootItem = try? JSONDecoder().decode(RootItem.self, from: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
 
-		return .success([])
+		return .success(rootItem.feedImages)
 	}
 }
