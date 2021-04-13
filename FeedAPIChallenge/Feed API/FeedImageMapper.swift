c30c2127a1dc775e183cd4d5dc640f9acad150b7
Vladimir Jeremic <vladimir@ezderm.com>
2021-04-13 17:53:44 +0200

    RemoteFeedLoder delivers error on partially valid JSON

--- FeedAPIChallenge/Feed API/FeedImageMapper.swift
+++ FeedAPIChallenge/Feed API/FeedImageMapper.swift
@@ -9,11 +9,22 @@
 import Foundation
 
 internal final class FeedImageMapper {
+	private struct RootItem: Decodable {
+		let items: [FeedImageItem]
+	}
+
+	private struct FeedImageItem: Decodable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
+	}
+
 	private static let OK_200_HTTP_SATUS_CODE = 200
 
 	internal static func map(_ data: Data, from response: HTTPURLResponse) -> FeedLoader.Result {
 		guard FeedImageMapper.OK_200_HTTP_SATUS_CODE == response.statusCode,
-		      let _ = try? JSONSerialization.jsonObject(with: data) else {
+		      let _ = try? JSONDecoder().decode(RootItem.self, from: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
 
