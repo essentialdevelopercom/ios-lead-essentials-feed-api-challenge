d685b8f6a228475cc1b8238666ecc49d7b5520bd
Adrian Bilescu <adrian.bilescu@gmail.com>
2021-04-15 14:07:40 +0300

    Implemented failure cases for the load method

--- Tests/LoadFeedFromRemoteUseCaseTests.swift
+++ Tests/LoadFeedFromRemoteUseCaseTests.swift
@@ -24,65 +24,65 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
 		XCTAssertTrue(client.requestedURLs.isEmpty)
 	}
 
-//	func test_loadTwice_requestsDataFromURLTwice() {
-//		let url = URL(string: "https://a-given-url.com")!
-//		let (sut, client) = makeSUT(url: url)
-//
-//		sut.load { _ in }
-//		sut.load { _ in }
-//
-//		XCTAssertEqual(client.requestedURLs, [url, url])
-//	}
-//
-//	func test_load_deliversConnectivityErrorOnClientError() {
-//		let (sut, client) = makeSUT()
-//
-//		expect(sut, toCompleteWith: .failure(.connectivity), when: {
-//			let clientError = NSError(domain: "Test", code: 0)
-//			client.complete(with: clientError)
-//		})
-//	}
-//
-//	func test_load_deliversInvalidDataErrorOnNon200HTTPResponse() {
-//		let (sut, client) = makeSUT()
-//
-//		let samples = [199, 201, 300, 400, 500]
-//
-//		samples.enumerated().forEach { index, code in
-//			expect(sut, toCompleteWith: .failure(.invalidData), when: {
-//				let json = makeItemsJSON([])
-//				client.complete(withStatusCode: code, data: json, at: index)
-//			})
-//		}
-//	}
-//
-//	func test_load_deliversInvalidDataErrorOn200HTTPResponseWithInvalidJSON() {
-//		let (sut, client) = makeSUT()
-//
-//		expect(sut, toCompleteWith: .failure(.invalidData), when: {
-//			let invalidJSON = Data("invalid json".utf8)
-//			client.complete(withStatusCode: 200, data: invalidJSON)
-//		})
-//	}
-//
-//	func test_load_deliversInvalidDataErrorOn200HTTPResponseWithPartiallyValidJSONItems() {
-//		let (sut, client) = makeSUT()
-//
-//		let validItem = makeItem(
-//			id: UUID(),
-//			imageURL: URL(string: "http://another-url.com")!
-//		).json
-//
-//		let invalidItem = ["invalid": "item"]
-//
-//		let items = [validItem, invalidItem]
-//
-//		expect(sut, toCompleteWith: .failure(.invalidData), when: {
-//			let json = makeItemsJSON(items)
-//			client.complete(withStatusCode: 200, data: json)
-//		})
-//	}
-//
+	func test_loadTwice_requestsDataFromURLTwice() {
+		let url = URL(string: "https://a-given-url.com")!
+		let (sut, client) = makeSUT(url: url)
+
+		sut.load { _ in }
+		sut.load { _ in }
+
+		XCTAssertEqual(client.requestedURLs, [url, url])
+	}
+
+	func test_load_deliversConnectivityErrorOnClientError() {
+		let (sut, client) = makeSUT()
+
+		expect(sut, toCompleteWith: .failure(.connectivity), when: {
+			let clientError = NSError(domain: "Test", code: 0)
+			client.complete(with: clientError)
+		})
+	}
+
+	func test_load_deliversInvalidDataErrorOnNon200HTTPResponse() {
+		let (sut, client) = makeSUT()
+
+		let samples = [199, 201, 300, 400, 500]
+
+		samples.enumerated().forEach { index, code in
+			expect(sut, toCompleteWith: .failure(.invalidData), when: {
+				let json = makeItemsJSON([])
+				client.complete(withStatusCode: code, data: json, at: index)
+			})
+		}
+	}
+
+	func test_load_deliversInvalidDataErrorOn200HTTPResponseWithInvalidJSON() {
+		let (sut, client) = makeSUT()
+
+		expect(sut, toCompleteWith: .failure(.invalidData), when: {
+			let invalidJSON = Data("invalid json".utf8)
+			client.complete(withStatusCode: 200, data: invalidJSON)
+		})
+	}
+
+	func test_load_deliversInvalidDataErrorOn200HTTPResponseWithPartiallyValidJSONItems() {
+		let (sut, client) = makeSUT()
+
+		let validItem = makeItem(
+			id: UUID(),
+			imageURL: URL(string: "http://another-url.com")!
+		).json
+
+		let invalidItem = ["invalid": "item"]
+
+		let items = [validItem, invalidItem]
+
+		expect(sut, toCompleteWith: .failure(.invalidData), when: {
+			let json = makeItemsJSON(items)
+			client.complete(withStatusCode: 200, data: json)
+		})
+	}
+
 //	func test_load_deliversSuccessWithNoItemsOn200HTTPResponseWithEmptyJSONList() {
 //		let (sut, client) = makeSUT()
 //
