dcdd78ae554f1f3a4327b73251372c4b1f72d70b
Adrian Bilescu <adrian.bilescu@gmail.com>
2021-04-15 14:22:32 +0300

    Implemented API Remote feed item structure to filter out invalid json responses

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,11 +21,40 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
+			case let .success((data, response)):
+				completion(RemoteFeedItemsMapper.map(data, from: response))
 			case .failure:
 				completion(.failure(Error.connectivity))
-			default:
-				completion(.failure(Error.invalidData))
 			}
 		}
+	}
+}
+
+private struct RemoteFeedItemsMapper {
+	struct Root: Decodable {
+		let items: [Item]
+	}
+
+	struct Item: Decodable {
+		enum CodingKey: String {
+			case id = "image_id"
+			case desc = "image_desc"
+			case loc = "image_loc"
+			case url = "image_url"
+		}
+
+		let id: UUID
+		let desc: String?
+		let loc: String?
+		let url: URL
+	}
+
+	static func map(_ data: Data, from response: HTTPURLResponse) -> FeedLoader.Result {
+		guard response.statusCode == 200,
+		      (try? JSONDecoder().decode(Root.self, from: data)) != nil
+		else {
+			return .failure(RemoteFeedLoader.Error.invalidData)
+		}
+		return .success([])
 	}
 }
