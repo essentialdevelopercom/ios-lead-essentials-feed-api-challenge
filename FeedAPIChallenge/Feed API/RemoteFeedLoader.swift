e074109f97e3c4d95d39964a8dc3fd0c6233696d
Onyekachi-152 <ezeokeonyekachi@gmail.com>
2021-04-16 11:40:59 +0100

    test that load request delivers connectivity error on the client

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,6 +20,12 @@ public final class RemoteFeedLoader: FeedLoader {
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
+			switch result {
+			case .success:
+				completion(.success([]))
+			case .failure:
+				completion(.failure(RemoteFeedLoader.Error.connectivity))
+			}
 		}
 	}
 }
