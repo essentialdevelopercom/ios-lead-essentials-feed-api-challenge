0fda787b9ca701623e80fdd0acc9490ff70b484d
224Apps <224apps@gmail.com>
2021-04-17 16:40:52 -0400

    Updated the `FeedImageMapper` and its method `map` to internal

--- FeedAPIChallenge/Feed API/FeedImageMapper.swift
+++ FeedAPIChallenge/Feed API/FeedImageMapper.swift
@@ -8,14 +8,14 @@
 
 import Foundation
 
-public struct FeedImageMapper {
+internal struct FeedImageMapper {
 	struct Root: Codable {
 		let items: [FeedImageItem]
 		public var feedImages: [FeedImage] {
 			return items.map { $0.feedImage }
 		}
 	}
-
+	
 	struct FeedImageItem: Codable {
 		let image_id: UUID
 		let image_desc: String?
@@ -25,15 +25,15 @@ public struct FeedImageMapper {
 			return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
 		}
 	}
-
+	
 	private static var OK_200 = 200
-
-	public static func map(data: Data, from response: HTTPURLResponse) -> FeedLoader.Result {
+	
+	internal static func map(data: Data, from response: HTTPURLResponse) -> FeedLoader.Result {
 		guard response.statusCode == OK_200,
-		      let root = try? JSONDecoder().decode(Root.self, from: data) else {
+			  let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
-
+		
 		return .success(root.feedImages)
 	}
 }
