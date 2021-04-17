47d05e5f5f0eb83413314eb805c63fad76b4eea6
oligazar <nikelodian@gmail.com>
2021-04-17 17:51:52 +0300

    simplify success case handling expression

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,12 +22,8 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { result in
 			switch result {
 			case let .success((data, response)):
-				if response.statusCode == 200 {
-					if let _ = try? JSONDecoder().decode(Root.self, from: data) {
-						completion(FeedLoader.Result.success([]))
-					} else {
-						completion(FeedLoader.Result.failure(Error.invalidData))
-					}
+				if let _ = try? JSONDecoder().decode(Root.self, from: data), response.statusCode == 200 {
+					completion(FeedLoader.Result.success([]))
 				} else {
 					completion(FeedLoader.Result.failure(Error.invalidData))
 				}
