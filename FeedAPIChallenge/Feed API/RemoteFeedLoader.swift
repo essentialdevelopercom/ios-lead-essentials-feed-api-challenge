be2784baec7c3111c55e93f93aaeb3735ad32884
oligazar <nikelodian@gmail.com>
2021-04-17 18:49:53 +0300

    Move FeedImageMapper to it's own file

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -36,36 +36,4 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 }
 
-private class FeedImageMapper {
-	private struct Root: Decodable {
-		let items: [Item]
-	}
-
-	private struct Item: Decodable {
-		let id: UUID
-		let description: String?
-		let location: String?
-		let url: URL
-		
-		private enum CodingKeys: String, CodingKey {
-			case id = "image_id"
-			case description = "image_desc"
-			case location = "image_loc"
-			case url = "image_url"
-		}
-		
-		var image: FeedImage {
-			return FeedImage(id: id, description: description, location: location, url: url)
-		}
-	}
-	
-	static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
-		guard response.statusCode == 200 else {
-			throw RemoteFeedLoader.Error.invalidData
-		}
-		let root = try JSONDecoder().decode(Root.self, from: data)
-		return root.items.map { $0.image }
-	}
-}
-
 
