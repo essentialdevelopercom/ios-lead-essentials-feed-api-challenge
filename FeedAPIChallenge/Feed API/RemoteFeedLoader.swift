9697503a12cbf89c4fecda36dd8406ecc641b49d
ryanzzff <ryanzzff@gmail.com>
2021-04-18 00:13:54 +0800

    Do not deliver result after `RemoteFeedLoader` deallocated

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,7 +19,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url, completion: { result in
+		client.get(from: url, completion: { [weak self] result in
+			guard self != nil else { return }
 			switch result {
 			case .success((let data, let response)):
 				guard response.statusCode == 200 else {
