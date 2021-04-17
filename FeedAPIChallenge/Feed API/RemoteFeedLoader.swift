9ade38eb0c195566001aee11447821257fd3b9d5
ryanzzff <ryanzzff@gmail.com>
2021-04-17 23:09:25 +0800

    Deliver error for invalid json

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,8 +21,10 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url, completion: { result in
 			switch result {
-			case .success((_, let response)):
+			case .success((let data, let response)):
 				if response.statusCode != 200 {
+					completion(.failure(Error.invalidData))
+				} else if !JSONSerialization.isValidJSONObject(data) {
 					completion(.failure(Error.invalidData))
 				}
 			case .failure:
