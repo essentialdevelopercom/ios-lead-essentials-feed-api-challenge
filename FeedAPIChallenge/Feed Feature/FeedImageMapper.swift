7cf27256031dd91630478db086dee66c736fa0d3
Alan Casas <alancasasarevalo@gmail.com>
2021-04-19 12:10:33 +0200

    [FEATURE][URLSessionHTTPClient] GREEN CIRCLE PHASE: * Ensure that we received correct values mapped when received a correct data

--- FeedAPIChallenge/Feed Feature/FeedImageMapper.swift
+++ FeedAPIChallenge/Feed Feature/FeedImageMapper.swift
@@ -3,20 +3,24 @@ import Foundation
 internal final class FeedImageMapper {
 	private struct Root: Decodable {
 		let items: [ImageItem]
+
+		var imageFeed: [FeedImage] {
+			return items.map { $0.item }
+		}
 	}
 
 	private struct ImageItem: Decodable {
 		let image_id: UUID
-		let image_description: String?
-		let image_location: String?
+		let image_desc: String?
+		let image_loc: String?
 		let image_url: URL
 
-		var item: ImageItem {
-			return ImageItem(
-				image_id: image_id,
-				image_description: image_description,
-				image_location: image_location,
-				image_url: image_url
+		var item: FeedImage {
+			return FeedImage(
+				id: image_id,
+				description: image_desc,
+				location: image_loc,
+				url: image_url
 			)
 		}
 	}
@@ -24,10 +28,10 @@ internal final class FeedImageMapper {
 	private static var OK_200: Int { return 200 }
 
 	internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-		guard response.statusCode == OK_200, let _ = try? JSONDecoder().decode(Root.self, from: data) else {
+		guard response.statusCode == OK_200, let rootImageItems = try? JSONDecoder().decode(Root.self, from: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
 
-		return .success([])
+		return .success(rootImageItems.imageFeed)
 	}
 }
