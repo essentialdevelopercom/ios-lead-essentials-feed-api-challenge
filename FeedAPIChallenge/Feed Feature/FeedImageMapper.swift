03641df5ed628e44284b95d9b33077c7fc72b100
Alan Casas <alancasasarevalo@gmail.com>
2021-04-19 11:15:19 +0200

    [FEATURE][URLSessionHTTPClient] GREEN CIRCLE PHASE: * Ensure that if mapper receive invalid data will return invalid data error

--- FeedAPIChallenge/Feed Feature/FeedImageMapper.swift
+++ FeedAPIChallenge/Feed Feature/FeedImageMapper.swift
@@ -4,7 +4,7 @@ internal final class FeedImageMapper {
 	private static var OK_200: Int { return 200 }
 
 	internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-		guard response.statusCode == OK_200 else {
+		guard response.statusCode == OK_200, let _ = try? JSONSerialization.jsonObject(with: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
 
