a0ad4248a3ea08e6913969e4b5efea323286503a
Vita Solomina <vita@parsable.com>
2021-04-20 20:45:27 -0700

    load delivers connectivity error on client error

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,7 +19,13 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { _ in
+		client.get(from: url) { result in
+			switch result {
+			case .success(_):
+				break
+			case .failure(_):
+				completion(.failure(Error.connectivity))
+			}
 		}
 	}
 }
