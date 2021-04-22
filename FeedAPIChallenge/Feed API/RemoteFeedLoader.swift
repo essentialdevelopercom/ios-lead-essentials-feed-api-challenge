a08d1a73207ad1cc8bebfc09541493822b8ea1d2
Vita Solomina <vita@parsable.com>
2021-04-21 19:22:21 -0700

    Remove CodingKeys to model exact API response

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -49,20 +49,13 @@ private struct RemoteFeedImageMapper {
 }
 
 private struct RemoteFeedImage: Decodable {
-	enum CodingKeys: String, CodingKey {
-		case id = "image_id"
-		case description = "image_desc"
-		case location = "image_loc"
-		case url = "image_url"
-	}
-
-	let id: UUID
-	let description: String?
-	let location: String?
-	let url: URL
+	let image_id: UUID
+	let image_desc: String?
+	let image_loc: String?
+	let image_url: URL
 
 	func toFeedImage() -> FeedImage {
-		return FeedImage(id: id, description: description, location: location, url: url)
+		return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
 	}
 }
 
