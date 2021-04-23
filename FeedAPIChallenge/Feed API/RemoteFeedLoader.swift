cc7a28c8f9f00ce258b30b65a7243109a69510dc
shreya-90 <shreya.90@gmail.com>
2021-04-24 00:33:49 +0530

    extra line removed
    Co-authored-by: danillahtin <danil_lahtin@mail.ru>

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,7 +20,6 @@ public final class RemoteFeedLoader: FeedLoader {
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { [weak self] result in
-            
             guard self != nil else { return }
             
             switch result {
