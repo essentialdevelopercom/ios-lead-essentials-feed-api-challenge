7bc1573bc0364a338aa42a25afe18c1d1bfd31dc
Stefano Martinallo <stefano.martinallo@urmet.com>
2021-04-27 16:17:53 +0200

    Mapping the Data to FeedItem

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -8,6 +8,20 @@ public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
 
+	private struct Root: Decodable {
+		let items: [Item]
+		var feedImages: [FeedImage] {
+			return items.map { FeedImage(id: $0.image_id, description: $0.image_desc, location: $0.image_loc, url: $0.image_url) }
+		}
+	}
+
+	private struct Item: Decodable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
+	}
+
 	public enum Error: Swift.Error {
 		case connectivity
 		case invalidData
@@ -22,15 +36,19 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { [weak self] result in
 			guard let self = self else { return }
 			switch result {
-			case .success:
-				completion(self.map())
+			case let .success((data, response)):
+				completion(self.map(data, and: response))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
 	}
 
-	private func map() -> FeedLoader.Result {
-		return .failure(Error.invalidData)
+	private func map(_ data: Data, and response: HTTPURLResponse) -> FeedLoader.Result {
+		guard
+			response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data)
+		else { return .failure(Error.invalidData) }
+
+		return .success(root.feedImages)
 	}
 }
