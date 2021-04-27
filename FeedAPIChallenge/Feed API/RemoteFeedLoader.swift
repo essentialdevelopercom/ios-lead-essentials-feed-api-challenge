4e9321c0fb5cf80cd4bfbb02ed0a1b87e451f20b
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-04-28 01:10:17 +0300

    load, delivers success with no items on 200 HTTP response

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,10 +20,17 @@ public final class RemoteFeedLoader: FeedLoader {
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
-			if case .success((_, _)) = result {
-				completion(.failure(Error.invalidData))
-			} else {
+			
+			switch result {
+			case .failure:
 				completion(.failure(Error.connectivity))
+				
+				case let .success((data, response)):
+					if response.statusCode == 200, let _ = try? JSONSerialization.jsonObject(with: data) {
+						completion(.success([]))
+					} else {
+						completion(.failure(Error.invalidData))
+					}
 			}
 		}
 	}
