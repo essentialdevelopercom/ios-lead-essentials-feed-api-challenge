58c79e1e79a833de8bf41f0eea0883148f5c3718
Guven Karanfil <guvenkaranfil235@gmail.com>
2021-04-28 01:15:32 +0300

    load, delivers successful with items on 200 HTTP response and valid json items

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -26,12 +26,27 @@ public final class RemoteFeedLoader: FeedLoader {
 				completion(.failure(Error.connectivity))
 				
 				case let .success((data, response)):
-					if response.statusCode == 200, let _ = try? JSONSerialization.jsonObject(with: data) {
-						completion(.success([]))
+					if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
+						completion(.success(root.items.map {$0.item }))
 					} else {
 						completion(.failure(Error.invalidData))
 					}
 			}
 		}
+	}
+}
+
+private struct Root: Decodable {
+	let items: [Item]
+}
+
+private struct Item: Decodable {
+	let image_id: UUID
+	let image_desc: String?
+	let image_loc: String?
+	let image_url: URL
+	
+	var item: FeedImage {
+		return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
 	}
 }
