309f7db47ece4ddf553832c9acfd25ac804edb61
therealchriswong <chriswong20@gmail.com>
2021-04-28 18:52:58 -0400

    test(remoteFeedLoader): load 2 request

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,6 +19,12 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		fatalError("Must be implemented")
+		client.get(from: url) {
+			[weak self] result in
+
+			guard self != nil else { return }
+
+			// completion
+		}
 	}
 }
