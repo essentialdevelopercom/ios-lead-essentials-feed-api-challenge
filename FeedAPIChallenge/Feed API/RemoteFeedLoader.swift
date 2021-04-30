0514839ee51cf262fda6b7cdee0194c665a1989e
therealchriswong <chriswong20@gmail.com>
2021-04-29 22:00:48 -0400

    test: client returns an Error.connectivity

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,12 +19,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) {
-			[weak self] result in
-
-			guard self != nil else { return }
-
-			// completion
+		client.get(from: url) { _ in
+			completion(.failure(Error.connectivity))
 		}
 	}
 }
