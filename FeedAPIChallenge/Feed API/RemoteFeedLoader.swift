682a05d23f751df647f5e3b726beeab2b211b38a
Michel Goñi <michelgoni@MBP-de-Michel.lan>
2021-05-01 18:37:12 +0200

    Delivers success with no items on 200 HTTPResponse

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,12 +21,39 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success((_, _)):
-				completion(.failure(Error.invalidData))
-				
+			case .success((let data, let response)):
+				if response.statusCode == 200,
+				   let _ = try? JSONDecoder().decode(Root.self, from: data) {
+					completion(.success([]))
+				}else {
+					completion(.failure(Error.invalidData))
+				}
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
 	}
 }
+
+private struct Root: Decodable {
+	let items: [Item]
+	
+	var feedImages: [FeedImage] {
+		 items.map { $0.feedImage }
+	}
+}
+
+private struct Item: Decodable {
+	let image_id: UUID
+	let image_desc: String?
+	let image_loc: String?
+	let image_url: URL
+	
+	var feedImage: FeedImage {
+		 FeedImage(id: image_id,
+						 description: image_desc,
+						 location: image_loc,
+						 url: image_url)
+	}
+}
+
