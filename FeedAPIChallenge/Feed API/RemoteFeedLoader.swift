5425adf5be3b84839fc32d365fce15d7e81ecc88
Michel Goñi <michelgoni@MBP-de-Michel.lan>
2021-05-03 10:54:36 +0200

    PR Changes

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,9 +20,9 @@ public final class RemoteFeedLoader: FeedLoader {
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { [weak self] result in
-			guard self != nil else {return}
+			guard self != nil else { return }
 			switch result {
-			case .success((let data, let response)):
+			case let .success((data, response)):
 				completion(FeedItemsMapper.map(data, from: response))
 			case .failure:
 				completion(.failure(Error.connectivity))
@@ -46,16 +46,17 @@ private struct Item: Decodable {
 	let image_url: URL
 	
 	var feedImage: FeedImage {
-		FeedImage(id: image_id,
-				  description: image_desc,
-				  location: image_loc,
-				  url: image_url)
+		FeedImage(
+			id: image_id,
+			description: image_desc,
+			location: image_loc,
+			url: image_url)
 	}
 }
 
-internal final class FeedItemsMapper {
+private class FeedItemsMapper {
 	private static var OK_200: Int { 200 }
-	internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+	static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
 		guard response.statusCode == OK_200,
 			  let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData)
