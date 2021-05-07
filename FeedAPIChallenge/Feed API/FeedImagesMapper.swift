30e99bb2ffbe7505f88e14509f69c2039a25de68
arupsarkar <sarkar.arup@gmail.com>
2021-05-07 00:38:07 -0400

    Delivers http response with 200 and also valid JSON. Since the test class had different json keys, had to enumerate the coding keys to map to correct keys of the data model.

--- FeedAPIChallenge/Feed API/FeedImagesMapper.swift
+++ FeedAPIChallenge/Feed API/FeedImagesMapper.swift
@@ -11,26 +11,52 @@ import Foundation
 internal final class FeedImagesMapper {
 	private struct Root: Decodable {
 		let items: [Item]
+		var feed: [FeedImage] {
+			return items.map({ $0.item })
+		}
 	}
 
 	private struct Item: Decodable {
 		let id: UUID //Required
 		let description: String? //Optional String
 		let location: String? //optional
-		let image: URL
+		let url: URL
 
 		var item: FeedImage {
-			return FeedImage(id: id, description: description, location: location, url: image)
+			return FeedImage(id: id, description: description, location: location, url: url)
+		}
+
+		enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case url = "image_url"
 		}
 	}
 
 	private static var OK_200: Int { return 200 }
 
 	internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+		do {
+			let result: Root
+			try result = JSONDecoder().decode(Root.self, from: data)
+			print(result)
+		} catch DecodingError.keyNotFound(let key, let context) {
+			Swift.print("Error: could not find key \(key) in JSON: \(context.debugDescription)")
+		} catch DecodingError.valueNotFound(let type, let context) {
+			Swift.print("Error: could not find type \(type) in JSON: \(context.debugDescription)")
+		} catch DecodingError.typeMismatch(let type, let context) {
+			Swift.print("Error: type mismatch for type \(type) in JSON: \(context.debugDescription)")
+		} catch DecodingError.dataCorrupted(let context) {
+			Swift.print("Error: data found to be corrupted in JSON: \(context.debugDescription)")
+		} catch let error as NSError {
+			NSLog("Error in read(from:ofType:) domain= \(error.domain), description= \(error.localizedDescription)")
+		}
+
 		guard response.statusCode == OK_200,
 		      let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
-		return .success(root.items.map { $0.item })
+		return .success(root.feed)
 	}
 }
