1f6c1f031cf276bf08565b638b88b502007721d4
arupsarkar <sarkar.arup@gmail.com>
2021-05-07 00:41:42 -0400

    Removed code to identify the problem in JSONDecoder, found out that the image_id, image_desc, image_loc, and image_url had to map to id, description, location and url.

--- FeedAPIChallenge/Feed API/FeedImagesMapper.swift
+++ FeedAPIChallenge/Feed API/FeedImagesMapper.swift
@@ -37,22 +37,6 @@ internal final class FeedImagesMapper {
 	private static var OK_200: Int { return 200 }
 
 	internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-		do {
-			let result: Root
-			try result = JSONDecoder().decode(Root.self, from: data)
-			print(result)
-		} catch DecodingError.keyNotFound(let key, let context) {
-			Swift.print("Error: could not find key \(key) in JSON: \(context.debugDescription)")
-		} catch DecodingError.valueNotFound(let type, let context) {
-			Swift.print("Error: could not find type \(type) in JSON: \(context.debugDescription)")
-		} catch DecodingError.typeMismatch(let type, let context) {
-			Swift.print("Error: type mismatch for type \(type) in JSON: \(context.debugDescription)")
-		} catch DecodingError.dataCorrupted(let context) {
-			Swift.print("Error: data found to be corrupted in JSON: \(context.debugDescription)")
-		} catch let error as NSError {
-			NSLog("Error in read(from:ofType:) domain= \(error.domain), description= \(error.localizedDescription)")
-		}
-
 		guard response.statusCode == OK_200,
 		      let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData)
