64d7e36860405835f647fd19241e6eb452d7fb4d
arupsarkar <sarkar.arup@gmail.com>
2021-05-06 21:53:56 -0400

    Production implementation delivers response code 200 and an empty array which matches with the test expectation.

--- FeedAPIChallenge/Feed API/FeedImagesMapper.swift
+++ FeedAPIChallenge/Feed API/FeedImagesMapper.swift
@@ -9,7 +9,28 @@
 import Foundation
 
 internal final class FeedImagesMapper {
+	private struct Root: Decodable {
+		let items: [Item]
+	}
+
+	private struct Item: Decodable {
+		let id: UUID //Required
+		let description: String? //Optional String
+		let location: String? //optional
+		let image: URL
+
+		var item: FeedImage {
+			return FeedImage(id: id, description: description, location: location, url: image)
+		}
+	}
+
+	private static var OK_200: Int { return 200 }
+
 	internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-		return .failure(RemoteFeedLoader.Error.invalidData)
+		guard response.statusCode == OK_200,
+		      let root = try? JSONDecoder().decode(Root.self, from: data) else {
+			return .failure(RemoteFeedLoader.Error.invalidData)
+		}
+		return .success(root.items.map { $0.item })
 	}
 }
