03087c70787ada9d5ec527be69b60988265e2fa2
marcos-reboucas <contact@marcosreboucas.com>
2021-05-11 08:35:45 -0300

    Add FeedImageMapper to decode JSON in a separate Item struct to let FeedItem decoupled from API implementation. Guarantee 'load' method will fail with invalid data if JSON received is partially valid and can't be decoded.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,13 +22,52 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { result in
 			switch result {
 			case let .success((data, response)):
-				guard response.statusCode == 200, let _ = try? JSONSerialization.jsonObject(with: data) else {
-					completion(.failure(Error.invalidData))
-					return
-				}
+				completion(FeedImageMapper.map(data, from: response))
 			case .failure(_):
 				completion(.failure(Error.connectivity))
 			}
 		}
+	}
+}
+
+private final class FeedImageMapper {
+	private struct Root: Decodable {
+		let items: [Item]
+
+		var imageFeed: [FeedImage] {
+			return items.map { $0.item }
+		}
+	}
+
+	private struct Item: Decodable {
+		let id: UUID
+		let description: String?
+		let location: String?
+		let imageURL: URL
+
+		enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case imageURL = "image_url"
+		}
+
+		var item: FeedImage {
+			return FeedImage(
+				id: id,
+				description: description,
+				location: location,
+				url: imageURL)
+		}
+	}
+
+	private static var statusCode_200: Int { return 200 }
+
+	internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+		guard response.statusCode == statusCode_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+			return .failure(RemoteFeedLoader.Error.invalidData)
+		}
+
+		return .success(root.imageFeed)
 	}
 }
