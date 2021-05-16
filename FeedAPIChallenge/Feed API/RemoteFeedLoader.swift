d5370e8f4635a86cbfffde0f4cfa4370540119e2
manivelnagarajan <write2manivel@gmail.com>
2021-05-16 20:02:41 +0530

    Validate the response data and completes with empty data

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,11 +21,35 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success:
-				completion(.failure(Error.invalidData))
+			case let .success((data, response)):
+				guard response.statusCode == 200 else {
+					completion(.failure(Error.invalidData))
+					return
+				}
+				do {
+					let _ = try JSONDecoder().decode(Root.self, from: data)
+					completion(.success([]))
+				} catch {
+					completion(.failure(Error.invalidData))
+				}
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
+		}
+	}
+
+	private struct Root: Decodable {
+		var items: [Image]
+	}
+
+	private struct Image: Decodable {
+		var id: UUID
+		var description: String?
+		var location: String?
+		var url: URL
+
+		private enum CodingKeys: String, CodingKey {
+			case id = "image_id", description = "image_desc", location = "image_loc", url = "image_url"
 		}
 	}
 }
