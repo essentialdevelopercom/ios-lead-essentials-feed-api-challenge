2436a56638f2d775bc0f7f82ae4819519c85739f
manivelnagarajan <write2manivel@gmail.com>
2021-05-16 20:08:03 +0530

    RemoteFeedLoader completes with feed images

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,11 +22,11 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { result in
 			switch result {
 			case let .success((data, response)):
-				guard response.statusCode == 200, let _ = try? JSONDecoder().decode(Root.self, from: data) else {
+				guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
 					completion(.failure(Error.invalidData))
 					return
 				}
-				completion(.success([]))
+				completion(.success(root.feedImages))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
