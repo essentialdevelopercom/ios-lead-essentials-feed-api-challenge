a524d77f642ab6308364df63959d2f34f9c9dbd4
Saul Cortez Garcia <saul.cortez0@walmart.com>
2021-05-20 15:31:01 -0500

    Add implementation for invalid status code

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -7,6 +7,7 @@ import Foundation
 public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
+	private let code_200 = 200
 
 	public enum Error: Swift.Error {
 		case connectivity
@@ -19,11 +20,16 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+
+			guard let self = self else { return }
 
 			switch result {
-			case .success((_, _)):
-				print("success")
+			case .success((_, let response)):
+
+				if response.statusCode != self.code_200 {
+					completion(.failure(Error.invalidData))
+				}
 
 			case .failure(_):
 				completion(.failure(Error.connectivity))
