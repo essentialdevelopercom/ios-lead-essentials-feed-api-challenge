9996562773df021e2c4ef0fadf42e1b2894a680d
Saul Cortez Garcia <saul.cortez0@walmart.com>
2021-05-20 17:55:26 -0500

    Add test success response with items

---
+++ FeedAPIChallenge/Feed Feature/RemoteFeedImage.swift
@@ -0,0 +1,9 @@
+//
+//  RemoteFeedImage.swift
+//  FeedAPIChallenge
+//
+//  Created by Saul Cortez Garcia on 20/05/21.
+//  Copyright © 2021 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
