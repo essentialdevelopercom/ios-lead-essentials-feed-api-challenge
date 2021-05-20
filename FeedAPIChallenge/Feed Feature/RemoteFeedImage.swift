0a82c796f391f50c902378b432e7171f3a9807f0
Saul Cortez Garcia <saul.cortez0@walmart.com>
2021-05-20 18:02:10 -0500

    Add RemoteFeedImage to his own file

--- FeedAPIChallenge/Feed Feature/RemoteFeedImage.swift
+++ FeedAPIChallenge/Feed Feature/RemoteFeedImage.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+struct FeedImageMapper: Decodable {
+	let items: [Item]
+
+	var feedImage: [FeedImage] {
+		return items.map {
+			FeedImage(id: $0.id,
+			          description: $0.description,
+			          location: $0.location,
+			          url: $0.imageURL)
+		}
+	}
+}
+
+struct Item: Decodable {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+
+	enum CodingKeys: String, CodingKey {
+		case id = "image_id"
+		case description = "image_desc"
+		case location = "image_loc"
+		case imageURL = "image_url"
+	}
+}
