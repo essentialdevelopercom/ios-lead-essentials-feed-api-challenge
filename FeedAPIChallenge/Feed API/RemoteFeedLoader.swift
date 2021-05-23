ba79b94146704f97bf105d2b3cbbd7ea1fc64d64
Hoff Silva <hoff.silva@ebanx.com>
2021-05-23 19:54:33 -0300

    Add implementation of load to pass on test_load_deliversInvalidDataErrorOnNon200HTTPResponse.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -23,7 +23,10 @@ public final class RemoteFeedLoader: FeedLoader {
 			switch result {
 			case .failure:
 				completion(.failure(Error.connectivity))
-			case .success: ()
+			case .success(let response):
+				if response.1.statusCode != 200 {
+					completion(.failure(Error.invalidData))
+				}
 			}
 		}
 	}
