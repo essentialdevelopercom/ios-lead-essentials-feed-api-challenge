56e57fee571bc3aa5949af102c1c7ab251dd7d0c
Hoff Silva <hoff.silva@ebanx.com>
2021-05-24 08:27:03 -0300

    Remove coding keys from FeedItemAPI

--- FeedAPIChallenge/Feed API/FeedImageAPI.swift
+++ FeedAPIChallenge/Feed API/FeedImageAPI.swift
@@ -9,15 +9,8 @@
 import Foundation
 
 struct FeedImageAPI: Decodable {
-	let id: UUID
-	let description: String?
-	let location: String?
-	let url: URL
-
-	enum CodingKeys: String, CodingKey {
-		case id = "image_id"
-		case description = "image_desc"
-		case location = "image_loc"
-		case url = "image_url"
-	}
+	let image_id: UUID
+	let image_desc: String?
+	let image_loc: String?
+	let image_url: URL
 }
