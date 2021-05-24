5fb3a66df73bdb6ba4e86d9ceee5a3ef31a6eabe
Dima Tsurkan <d-link13@mail.ru>
2021-05-24 19:20:06 +0300

    extracted mapping logic to a helper class

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,44 +22,15 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { result in
 			switch result {
 			case .success((let data, let response)):
-				guard response.statusCode == 200 else {
+				do {
+					let images = try FeedImageMapper.map(data: data, response: response)
+					completion(.success(images))
+				} catch {
 					completion(.failure(Error.invalidData))
-					return
 				}
-				guard let root = try? JSONDecoder().decode(Root.self, from: data) else {
-					completion(.failure(Error.invalidData))
-					return
-				}
-				completion(.success(root.images))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
-	}
-}
-
-struct Root: Decodable {
-	let items: [FeedImageItem]
-
-	var images: [FeedImage] {
-		return items.map { $0.feedImage }
-	}
-}
-
-struct FeedImageItem: Decodable {
-	let id: UUID
-	let description: String?
-	let location: String?
-	let url: URL
-
-	enum CodingKeys: String, CodingKey {
-		case id = "image_id"
-		case description = "image_desc"
-		case location = "image_loc"
-		case url = "image_url"
-	}
-
-	var feedImage: FeedImage {
-		return FeedImage(id: id, description: description, location: location, url: url)
 	}
 }
