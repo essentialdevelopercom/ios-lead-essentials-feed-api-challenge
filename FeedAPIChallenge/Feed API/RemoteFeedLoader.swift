7e51f52bcfa3e4d87ac241e34514079ee447288a
Dima Tsurkan <d-link13@mail.ru>
2021-05-24 18:55:15 +0300

    uncommented load_deliversSuccessWithNoItemsOn200HTTPResponseWithEmptyJSONList and load_deliversSuccessWithItemsOn200HTTPResponseWithJSONItems tests, implemented json object parsing using helper type to decouple decoding logic and FeedImage implementation.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,11 +21,45 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success:
-				completion(.failure(Error.invalidData))
+			case .success((let data, let response)):
+				guard response.statusCode == 200 else {
+					completion(.failure(Error.invalidData))
+					return
+				}
+				guard let root = try? JSONDecoder().decode(Root.self, from: data) else {
+					completion(.failure(Error.invalidData))
+					return
+				}
+				completion(.success(root.images))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
+	}
+}
+
+struct Root: Decodable {
+	let items: [FeedImageItem]
+
+	var images: [FeedImage] {
+		return items.map { $0.feedImage }
+	}
+}
+
+struct FeedImageItem: Decodable {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let url: URL
+
+	enum CodingKeys: String, CodingKey {
+		case id = "image_id"
+		case description = "image_desc"
+		case location = "image_loc"
+		case url = "image_url"
+	}
+
+	var feedImage: FeedImage {
+		return FeedImage(id: id, description: description, location: location, url: url)
 	}
 }
