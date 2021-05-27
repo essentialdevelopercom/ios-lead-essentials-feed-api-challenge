7f5cf4ce2d47516ffbea67c0914086591a18688a
Jose <jalvarez1@costar.com>
2021-05-27 07:55:30 -0700

    delivers success with no items when 200 with empty JSON list.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,11 +21,29 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success:
-				completion(.failure(RemoteFeedLoader.Error.invalidData))
+			case let .success((data, response)) where response.statusCode == 200:
+				let decoder = JSONDecoder()
+				if (try? decoder.decode(Items.self, from: data)) != nil {
+					completion(.success([]))
+				} else {
+					completion(.failure(RemoteFeedLoader.Error.invalidData))
+				}
 			case .failure:
 				completion(.failure(RemoteFeedLoader.Error.connectivity))
+			case .success((_, _)):
+				completion(.failure(RemoteFeedLoader.Error.invalidData))
 			}
 		}
+	}
+
+	private class Items: Codable {
+		var items: [FeedItem]
+	}
+
+	private class FeedItem: Codable {
+		var image_id: String
+		var image_desc: String?
+		var image_loc: String?
+		var image_url: URL
 	}
 }
