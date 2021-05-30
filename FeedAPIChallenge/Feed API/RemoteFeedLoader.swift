deb59aa350c6f15339591ea27307b3740ee23806
Salar <salar.zarandi@icloud.com>
2021-05-30 17:20:55 +0800

    Get connectivity error on client error

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,6 +19,14 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url, completion: { _ in })
+		client.get(from: url, completion: { result in
+			switch result {
+			case .failure:
+				completion(.failure(Error.connectivity))
+			default:
+				break
+			}
+
+		})
 	}
 }
