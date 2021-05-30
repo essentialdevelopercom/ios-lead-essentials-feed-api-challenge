0a42f77cfa6f2f6437203ef1ae9760a809d2cbf7
Pol Piella <info@polpiellamusic.com>
2021-05-30 18:46:11 +0100

    Deliver invalid data error if data cannot be decoded. This is the first step to safely decoding data from the server

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,9 +21,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case let .success((_, response)):
-				guard response.statusCode == 200 else { completion(.failure(Error.invalidData)); return }
-				completion(.failure(Error.invalidData))
+			case let .success((data, response)):
+				guard let _ = try? JSONDecoder().decode(FeedImage.self, from: data), response.statusCode == 200 else { completion(.failure(Error.invalidData)); return }
 			case .failure: completion(.failure(Error.connectivity))
 			}
 		}
