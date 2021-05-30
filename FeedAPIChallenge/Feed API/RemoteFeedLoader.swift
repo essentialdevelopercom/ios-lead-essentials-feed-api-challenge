847f34240edc46ac8522639383d4e0809001a4be
Fei Wen <fnytrip@gmail.com>
2021-05-30 16:27:55 -0700

    Change magic number 200 to a more descriptive static property

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -7,6 +7,7 @@ import Foundation
 public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
+	static let OK_200: Int = 200
 	
 	public enum Error: Swift.Error {
 		case connectivity
@@ -24,7 +25,7 @@ public final class RemoteFeedLoader: FeedLoader {
 			case .failure:
 				completion(.failure(Error.connectivity))
 			case .success((_, let response)):
-				if response.statusCode != 200 {
+				if response.statusCode != RemoteFeedLoader.OK_200 {
 					completion(.failure(Error.invalidData))
 				} else {
 					completion(.success([FeedImage]()))
