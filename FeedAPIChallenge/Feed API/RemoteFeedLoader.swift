b6e789a9482962c4ec2552d55adfc40ec8be0c81
Salar <salar.zarandi@icloud.com>
2021-05-30 17:28:58 +0800

    Delivers invalid data when data is not a valid json

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,9 +21,15 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url, completion: { result in
 			switch result {
-			case let .success((_, response)):
+			case let .success((data, response)):
 				if response.statusCode != 200 {
 					completion(.failure(Error.invalidData))
+				} else {
+					do {
+						let _ = try JSONSerialization.jsonObject(with: data)
+					} catch {
+						completion(.failure(Error.invalidData))
+					}
 				}
 			case .failure:
 				completion(.failure(Error.connectivity))
