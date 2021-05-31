532ca74ddd741609a425b635da5bb1608b5ef858
prashantkorat <korat_prashant@yahoo.in>
2021-05-31 16:22:24 +0530

    Added test for checking that  does not delivers result after `SUT` instance has been deallocated by checking weak reference of `RemoteFeedLoader` instance and preventing further execution if nil.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,7 +21,9 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+			guard self != nil else { return }
+			
 			switch (result) {
 			case let .success((data, response)):
 				if let items = try? FeedImageMapper.map(data, response) {
