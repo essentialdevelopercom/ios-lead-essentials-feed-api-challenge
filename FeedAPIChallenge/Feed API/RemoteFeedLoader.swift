1c8f930f2b76d8a60430124459052491aa6b25e4
Istvan Balogh <istvan.balogh@homegate.ch>
2021-06-01 09:54:41 +0700

    Return invalid data error when response status code is not 200

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -7,6 +7,7 @@ import Foundation
 public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
+	private let OK_200 = 200
 
 	public enum Error: Swift.Error {
 		case connectivity
@@ -19,10 +20,13 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [unowned self] result in
 			switch result {
-			case .success((_, _)):
-				break
+			case .success((_, let response)):
+				guard response.statusCode == self.OK_200 else {
+					completion(.failure(RemoteFeedLoader.Error.invalidData))
+					return
+				}
 			case .failure(_):
 				completion(.failure(RemoteFeedLoader.Error.connectivity))
 			}
