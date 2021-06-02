8daf8122fb64a4ab514ef342658092f29e45afbb
nameky <suchbalance@qq.com>
2021-06-02 10:51:47 +0800

    Delivers invalid data error when non 200 HTTP response.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,7 +20,12 @@ public final class RemoteFeedLoader: FeedLoader {
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
-			completion(.failure(Error.connectivity))
+			switch result {
+			case .success:
+				completion(.failure(Error.invalidData))
+			default:
+				completion(.failure(Error.connectivity))
+			}
 		}
 	}
 }
