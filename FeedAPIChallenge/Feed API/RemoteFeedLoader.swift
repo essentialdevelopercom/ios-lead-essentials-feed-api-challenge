82441a226a75f86975024348d36827268763199d
Konstatin Bogdanov <>
2021-06-04 15:06:44 +0300

    test_load_deliversInvalidDataErrorOn200HTTPResponseWithInvalidJSON passed

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -7,22 +7,27 @@ import Foundation
 public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
+	private let mapper: FeedImageMapper
 
 	public enum Error: Swift.Error {
 		case connectivity
 		case invalidData
 	}
 
-	public init(url: URL, client: HTTPClient) {
+	public init(url: URL,
+	            client: HTTPClient) {
 		self.url = url
 		self.client = client
+		self.mapper = FeedImageJSONMapper()
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+			guard let self = self else { return }
 			switch result {
-			case .success(let data, let response):
-				if response.statusCode == 200 {
+			case .success((let data, let response)):
+				if response.statusCode == 200,
+				   let _ = try? self.mapper.map(data: data) {
 					return
 				}
 				completion(.failure(Error.invalidData))
