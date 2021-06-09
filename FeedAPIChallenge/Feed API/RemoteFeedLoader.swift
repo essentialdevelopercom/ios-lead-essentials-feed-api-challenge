889837a154d3644c941b4b667257ebe55e3e8895
YH Kung <bowser.kung@gmail.com>
2021-06-09 18:55:18 +0800

    Move mapping logic to a new type `FeedImageMapper`

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,12 +22,13 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { result in
 			switch result {
 			case let .success((data, response)):
-				if let root = try? JSONDecoder().decode(Root.self, from: data), response.statusCode == 200 {
-					let feedImages = root.items.map { $0.image }
-					completion(.success(feedImages))
-				} else {
+				do {
+					let images = try FeedImageMapper.map(data, response)
+					completion(.success(images))
+				} catch {
 					completion(.failure(Error.invalidData))
 				}
+
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
@@ -35,17 +36,29 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 }
 
-private struct Root: Decodable {
-	let items: [ImageItem]
-}
+private class FeedImageMapper {
+	private struct Root: Decodable {
+		let items: [ImageItem]
+	}
 
-private struct ImageItem: Decodable {
-	let image_id: UUID
-	let image_desc: String?
-	let image_loc: String?
-	let image_url: URL
+	private struct ImageItem: Decodable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
 
-	var image: FeedImage {
-		return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+		var image: FeedImage {
+			return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+		}
+	}
+
+	static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
+		guard response.statusCode == 200 else {
+			throw RemoteFeedLoader.Error.invalidData
+		}
+		let root = try JSONDecoder().decode(Root.self, from: data)
+		let feedImages = root.items.map { $0.image }
+
+		return feedImages
 	}
 }
