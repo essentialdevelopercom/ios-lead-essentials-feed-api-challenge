afeea86695a94fe6f3e0f255519e5fffcae3fd08
Hai Phung <hai.phung@peoplemedia.com>
2021-06-09 09:40:11 -0500

    PR feeback

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -27,19 +27,16 @@ public final class RemoteFeedLoader: FeedLoader {
 				completion(.failure(Error.connectivity))
 
 			case let .success((data, response)):
-				guard let images = FeedImagesMapper.map(data: data, response: response) else {
-					return completion(.failure(Error.invalidData))
-				}
-				completion(.success(images))
+				completion(FeedImagesMapper.map(data: data, response: response))
 			}
 		}
 	}
 }
 
-final class FeedImagesMapper {
+private final class FeedImagesMapper {
 	private init() {}
 
-	struct Root: Decodable {
+	private struct Root: Decodable {
 		let items: [RemoteFeedImage]
 	}
 
@@ -50,15 +47,15 @@ final class FeedImagesMapper {
 		let image_url: URL
 	}
 
-	static func map(data: Data, response: HTTPURLResponse) -> [FeedImage]? {
+	static func map(data: Data, response: HTTPURLResponse) -> FeedLoader.Result {
 		guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
-			return nil
+			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
-		return root.items.toModels()
+		return .success(root.items.toModels())
 	}
 }
 
-extension Array where Element == FeedImagesMapper.RemoteFeedImage {
+private extension Array where Element == FeedImagesMapper.RemoteFeedImage {
 	func toModels() -> [FeedImage] {
 		map { FeedImage(id: $0.image_id, description: $0.image_desc, location: $0.image_loc, url: $0.image_url) }
 	}
