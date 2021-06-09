3aed3689240a7bececc081147f48a84369708c59
Hai Phung <hai.phung@peoplemedia.com>
2021-06-09 00:36:22 -0500

    Moved image mapping logic to a separate FeedImageMapper component.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,21 +20,35 @@ public final class RemoteFeedLoader: FeedLoader {
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { [weak self] result in
-			guard let self = self else { return }
-
+			guard self != nil else { return }
+			
 			switch result {
 			case .failure:
 				completion(.failure(Error.connectivity))
+				
 			case let .success((data, response)):
-				guard response.statusCode == 200, let images = self.mapped(data: data, response: response) else {
+				guard response.statusCode == 200, let images = FeedImagesMapper.map(data: data, response: response) else {
 					return completion(.failure(Error.invalidData))
 				}
 				completion(.success(images))
 			}
 		}
 	}
+}
 
-	private func mapped(data: Data, response: HTTPURLResponse) -> [FeedImage]? {
+class FeedImagesMapper {
+	struct Root: Decodable {
+		let items: [RemoteFeedImage]
+	}
+
+	struct RemoteFeedImage: Decodable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
+	}
+	
+	static func map(data: Data, response: HTTPURLResponse) -> [FeedImage]? {
 		guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			return nil
 		}
@@ -42,18 +56,7 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 }
 
-private struct Root: Decodable {
-	let items: [RemoteFeedImage]
-}
-
-private struct RemoteFeedImage: Decodable {
-	let image_id: UUID
-	let image_desc: String?
-	let image_loc: String?
-	let image_url: URL
-}
-
-extension Array where Element == RemoteFeedImage {
+extension Array where Element == FeedImagesMapper.RemoteFeedImage {
 	func toModels() -> [FeedImage] {
 		map { FeedImage(id: $0.image_id, description: $0.image_desc, location: $0.image_loc, url: $0.image_url) }
 	}
