70b8556bf2f9b0fe9cd4f32e089708abcc76a213
Hai Phung <hai.phung@peoplemedia.com>
2021-06-09 00:25:48 -0500

    RemoteFeedLoader delivers empty array on 200 HTTP response with empty JSON list

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,23 +19,42 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+			guard let self = self else { return }
+			
 			switch result {
 			case .failure:
 				completion(.failure(Error.connectivity))
 			case let .success((data, response)):
-				guard response.statusCode == 200, let _ = try? JSONDecoder().decode([RemoteFeedImage].self, from: data) else {
+				guard response.statusCode == 200, let images = self.mapped(data: data, response: response) else {
 					return completion(.failure(Error.invalidData))
 				}
-				completion(.success([]))
+				completion(.success(images))
 			}
 		}
 	}
+	
+	private func mapped(data: Data, response: HTTPURLResponse) -> [FeedImage]? {
+		guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+			return nil
+		}
+		return root.items.toModels()
+	}
+}
 
-	private struct RemoteFeedImage: Decodable {
-		let image_id: UUID
-		let image_desc: String?
-		let image_loc: String?
-		let image_url: URL
+private struct Root: Decodable {
+	let items: [RemoteFeedImage]
+}
+
+private struct RemoteFeedImage: Decodable {
+	let image_id: UUID
+	let image_desc: String?
+	let image_loc: String?
+	let image_url: URL
+}
+
+extension Array where Element == RemoteFeedImage {
+	func toModels() -> [FeedImage] {
+		map { FeedImage(id: $0.image_id, description: $0.image_desc, location: $0.image_loc, url: $0.image_url) }
 	}
 }
