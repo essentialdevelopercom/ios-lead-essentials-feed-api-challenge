e05db15b5e2ce91a558ffc905ccb8d7fc5908ded
Luis Zapata <lalejzapata@falabella.cl>
2021-06-16 20:21:17 -0400

    Return invalid data scenario on httpCliente response different than 200

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,10 +21,12 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
+			case .success((_, let httpURLResponse)):
+				if httpURLResponse.statusCode != 200 {
+					completion(.failure(RemoteFeedLoader.Error.invalidData))
+				}
 			case .failure(_):
 				completion(.failure(RemoteFeedLoader.Error.connectivity))
-			default:
-				break
 			}
 		}
 	}
