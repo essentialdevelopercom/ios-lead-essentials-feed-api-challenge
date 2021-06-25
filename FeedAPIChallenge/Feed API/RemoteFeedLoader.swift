ab3808d8c21d20688b9b531fb23923df43ebfb82
Konstantin Kolontay <kkolontay@gmail.com>
2021-06-24 20:23:46 -0400

    test load doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -7,19 +7,20 @@ import Foundation
 public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
-
+	
 	public enum Error: Swift.Error {
 		case connectivity
 		case invalidData
 	}
-
+	
 	public init(url: URL, client: HTTPClient) {
 		self.url = url
 		self.client = client
 	}
-
+	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+			guard self != nil else { return }
 			switch result {
 			case .failure:
 				completion(.failure(Error.connectivity))
@@ -38,21 +39,22 @@ fileprivate struct FeedLoaderDecoder {
 	private struct FeedLoaderResult: Decodable {
 		let items: [FeedImageItem]
 	}
-
+	
 	private struct FeedImageItem: Decodable {
 		let image_id: String
 		let image_desc: String?
 		let image_loc: String?
 		let image_url: String
+		
 		var item: FeedImage? {
-			guard let url = URL(string: image_url) else {
+			guard let url = URL(string: image_url), let uuid = UUID(uuidString: image_id) else {
 				return nil
 			}
-			return FeedImage(id: (UUID(uuidString: image_id) ?? UUID()), description: image_desc, location: image_loc, url: url)
+			return FeedImage(id: uuid, description: image_desc, location: image_loc, url: url)
 		}
 	}
-
-	static func feedLoaderItemsDecoder(with data: Data) -> Swift.Result<[FeedImage], Error> {
+	
+	static func feedLoaderItemsDecoder( with data: Data) -> Swift.Result<[FeedImage], Error> {
 		do {
 			let decoder = JSONDecoder()
 			let feedLoader = try decoder.decode(FeedLoaderResult.self, from: data)
