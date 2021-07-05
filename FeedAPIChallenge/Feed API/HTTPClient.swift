7adff738d32427bcd92f228e363f5b5d2dc014c9
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2021-07-05 10:23:44 +0200

    Implement RemoteHTTPClient with share session.
    RemoteHTTPClient is a client, which will use the shared session to reply with the Result<[FeedImage], Error>
    
    Signed-off-by: Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>

--- FeedAPIChallenge/Feed API/HTTPClient.swift
+++ FeedAPIChallenge/Feed API/HTTPClient.swift
@@ -9,3 +9,27 @@ public protocol HTTPClient {
 
 	func get(from url: URL, completion: @escaping (Result) -> Void)
 }
+
+public class RemoteHTTPClient: HTTPClient {
+	private let session: URLSession
+
+	init(_ session: URLSession = .shared) {
+		self.session = session
+	}
+
+	enum RemoteHTTPClientError: Error {
+		case noDataNoResponseNoError
+	}
+
+	public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+		session.dataTask(with: url) { data, response, error in
+			if let error = error {
+				completion(.failure(error))
+			} else if let data = data, let response = response as? HTTPURLResponse {
+				completion(.success((data, response)))
+			} else {
+				completion(.failure(RemoteHTTPClientError.noDataNoResponseNoError))
+			}
+		}
+	}
+}
