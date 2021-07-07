41795fda1b8b96186c739121b0824a1220ee0ae3
Gowtham Namuri <gowtham.namuri@ymedialabs.com>
2021-07-07 08:22:26 +0530

    Added proper completion block for success case in RemoteFeedLoader class. To make some testCases to follow.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,11 +21,39 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success:
-				completion(.failure(Error.invalidData))
+			case let .success((data, response)):
+				guard response.statusCode == 200, let feedItemsRoot = try? JSONDecoder().decode(Root.self, from: data) else {
+					completion(.failure(Error.invalidData))
+					return
+				}
+				completion(.success(feedItemsRoot.feedImages))
 			default:
 				completion(.failure(Error.connectivity))
 			}
 		}
+	}
+}
+
+private struct Root: Decodable {
+	let items: [FeedItem]
+
+	var feedImages: [FeedImage] {
+		items.map({ FeedImage(id: $0.uuid, description: $0.description, location: $0.location, url: $0.imageURL) })
+	}
+}
+
+private struct FeedItem: Decodable {
+	let uuid: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
+
+extension FeedItem {
+	enum CodingKeys: String, CodingKey {
+		case uuid = "image_id"
+		case description = "image_desc"
+		case location = "image_loc"
+		case imageURL = "image_url"
 	}
 }
