d793ea018ea84fc9b71e8509715c0d87639da8ea
kshitij <kshitij.gupta@swiggy.in>
2021-07-11 20:07:07 +0530

    Handling Delivers Success With items on 200 HTTP Response with JSON Items

--- FeedAPIChallenge/Feed Feature/FeedImagesMapper.swift
+++ FeedAPIChallenge/Feed Feature/FeedImagesMapper.swift
@@ -22,13 +22,13 @@ final class FeedImagesMapper {
 	}
 
 	private struct Item: Decodable {
-		let id: UUID
-		let description: String?
-		let location: String?
-		let url: URL
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
 
 		var item: FeedImage {
-			return FeedImage(id: id, description: description, location: location, url: url)
+			return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
 		}
 	}
 
