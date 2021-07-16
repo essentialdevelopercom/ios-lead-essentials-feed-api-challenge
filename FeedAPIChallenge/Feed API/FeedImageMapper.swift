6d2449dd2950b0f04bb69f16328f832b57ff5fbc
AyazRahman <muhtasim.ayaz@gmail.com>
2021-07-16 12:51:46 +1000

    move Factory Class to a separate file and change the access modifier to internal as it should not be visible outside the module.

---
+++ FeedAPIChallenge/Feed API/FeedImageMapper.swift
@@ -0,0 +1,49 @@
+//
+//  FeedImageMapper.swift
+//  FeedAPIChallenge
+//
+//  Created by Ayaz Rahman on 16/7/21.
+//  Copyright © 2021 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
+
+internal struct FeedImageMapper {
+	private struct Root: Decodable {
+		let items: [Item]
+
+		var feedImages: [FeedImage] {
+			return items.map { $0.feedImage }
+		}
+	}
+
+	private struct Item: Decodable {
+		let id: UUID
+		let description: String?
+		let location: String?
+		let imageURL: URL
+
+		enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case imageURL = "image_url"
+		}
+
+		var feedImage: FeedImage {
+			return FeedImage(id: id, description: description, location: location, url: imageURL)
+		}
+	}
+
+	private static let OK_200: Int = 200
+
+	internal static func map(_ data: Data, from response: HTTPURLResponse) -> FeedLoader.Result {
+		guard response.statusCode == OK_200,
+			  let root = try? JSONDecoder().decode(Root.self, from: data)
+		else {
+			return .failure(RemoteFeedLoader.Error.invalidData)
+		}
+		return .success(root.feedImages)
+	}
+}
+
