c737d27e76793f1689550d0b187fe7c4df0df4a4
Yonic Surny <562587+aelptos@users.noreply.github.com>
2021-07-19 13:01:22 +0200

    Explicitly resolving `.failure` enum case instead of relying on `default`
    Co-authored-by: thiagopenna <51384849+thiagopenna@users.noreply.github.com>

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -24,7 +24,7 @@ public final class RemoteFeedLoader: FeedLoader {
 			switch result {
 			case let .success((data, response)):
 				completion(FeedImagesMapper.map(data, from: response))
-			default:
+			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
